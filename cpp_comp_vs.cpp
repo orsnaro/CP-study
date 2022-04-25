bd9c4594120f9c36c4f53591b6f62170adfe7593
orsnaro <omar1xd@gmail.com>
2022-04-25 21:43:54 +0200

    now its ready  just go file>open>file> choose .sln wich is inside local repo
    only run one .cpp at a time

--- cpp_comp_vs.cpp
+++ cpp_comp_vs.cpp
@@ -1,20 +1,19 @@
-// cpp_comp_vs.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+//============================================================================
+// Name        : Tour of CPP sheet
+// Author      : Omar.R.S
+// Version     :
+// Copyright   : Your copyright notice (<3)
+// Description : solving and  exercising learned things from "Tour of C++"
+//============================================================================
+
+
 
 #include <iostream>
+using namespace std;
 
-int main()
+int main(  )
 {
-    std::cout << "Hello World!\n";
+//1)try const refrence in other function and modify it + the actual variable (pg12 2nd star)
+
+    return (0);
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ tour_of_cpp.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	//1)try const refrence in other functionand modify it + the actual variable(pg12 2nd star)
+
+	return 0;
+}
+
