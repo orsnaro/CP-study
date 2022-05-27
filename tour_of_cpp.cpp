a8751fdb54137dcdea20f03776f1cea5c8ab89f0
orsnaro <omar1xd@gmail.com>
2022-05-28 00:36:19 +0200

    testingg githup form vscode not vsstudio :D

--- tour_of_cpp.cpp
+++ tour_of_cpp.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
 
-	cout << "hello world!" << endl;
+	cout << "hello world! omar's version" << endl;
 	//1)try const refrence in other functionand modify it + the actual variable(pg12 2nd star)
 
 	return 0;
