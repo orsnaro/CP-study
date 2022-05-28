e14da0a08d50660274587c00d3bc9a8e5a7cb582
orsnaro <omar1xd@gmail.com>
2022-05-28 10:31:26 +0200

    making vs code and studio and git hup has same remote

--- tour_of_cpp.cpp
+++ tour_of_cpp.cpp
@@ -6,10 +6,7 @@ int main() {
 
 	cout << "hello world! omar's version" << endl;
 	//1)try const refrence in other functionand modify it + the actual variable(pg12 2nd star)
-<<<<<<< HEAD
 //tst
 	return 0;
-=======
-return 0;
->>>>>>> f8eaeadd1fecff0a07def16964692fa89501a919
+
 }
