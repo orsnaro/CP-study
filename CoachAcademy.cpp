f01fef03629a91a0c38a30a29670bc92b8996ed9
orsnaro <omar1xd@gmail.com>
2022-07-25 04:42:44 +0200

    #

--- CoachAcademy.cpp
+++ CoachAcademy.cpp
@@ -1,16 +1,48 @@
 #include <bits/stdc++.h>
-#include <conio.h>
-
 
 using namespace std;
 
-int main(){
-	
-	
+vector<int> rotateLeft(int d, vector<int> arr);
 
-	
-	
-	
-	getch();
-	return 0;
+int main()
+{
+    int n = 0, d = 0;
+    cin >> n >> d;
+    vector<int> arr(n);
+    
+    arr = rotateLeft(d, arr);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << ' ';
+    }
+
+    return 0;
+}
+
+
+vector<int> rotateLeft(int d, vector<int> arr)
+{
+
+    int n = arr.size();
+
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < d; i++)
+    {
+
+        int temp = arr[0];
+
+        for (int j = 0; j < n; j++)
+        {
+            arr[j] = arr[j + 1];
+        }
+
+        arr[(n - 1)] = temp;
+    }
+
+    return arr;
 }
