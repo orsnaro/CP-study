e421385e83e10a946ac7629b27980a677e87e51c
omar rashad salem <77698890+orsnaro@users.noreply.github.com>
2022-07-29 17:33:00 +0200

    previous push was not main c.academy coding file + all codes r on vjudge

--- CoachAcademy.cpp
+++ CoachAcademy.cpp
@@ -1,51 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main()
-{
-     ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    
-    deque<int> dq_contnr;
-    string query;
-    int Q, N = 0;
 
-    scanf("%d", &Q);
-    while (Q--)
-    {
-        cin >> query;
-        if (query == "back")
-        {
-            if (!dq_contnr.empty())
-            {
-                cout << dq_contnr.back() << '\n';
-                dq_contnr.pop_back();
-            }
-            else
-                cout << "No job for Ada?" << '\n';
-        }
-        else if (query == "front")
-        {
-            if (!dq_contnr.empty())
-            {
-                cout << dq_contnr.front() << '\n';
-                dq_contnr.pop_front();
-            }
-            else
-                cout << "No job for Ada?" << '\n';
-        }
-        else if (query == "reverse")
-        {
-            reverse(dq_contnr.begin(), dq_contnr.end());
-        }
-        else if (query == "push_back")
-        {
-            scanf("%d", &N);
-            dq_contnr.push_back(N);
-        }
-        else
-        {
-            scanf("%d", &N);
-            dq_contnr.push_front(N);
-        }
+int main() {
+
+  int n;
+  cin >> n;
+cin.ignore();
+  while (n--) {
+    string temp;
+    getline(cin,temp);
+    stack <char> st;
+    bool valid = 1;
+
+    for (char c: temp) {
+
+      if (c == '[' || c == '(')
+        st.push(c);
+
+      else if (st.empty()) {
+        valid = 0;
+        break;
+      }
+
+      else if ((st.top()=='('&&c ==')') || (st.top()=='['&& c==']'))
+        st.pop();
+
+      else {
+        valid = 0;
+        break;
+      }
     }
-    return 0;}
+    cout << (valid && st.empty() ? "Yes\n" : "No\n");
+
+  }
+  return 0;
+}
