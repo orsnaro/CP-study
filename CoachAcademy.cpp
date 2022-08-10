142c6de509fd422f4e1be82565e8e933863c0b7a
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2022-08-10 21:08:14 +0200

    solved h greedy sheet

--- CoachAcademy.cpp
+++ CoachAcademy.cpp
@@ -13,42 +13,33 @@ int main() {
   cin.tie(nullptr);
   cout.tie(nullptr);
 
-int n ; cin >> n;
-while(n--){
+  int t,n,a,b;
+  cin >> t;
+  while(t--){
+    cin >> n;
+    cin >> a >> b;
+    string str ;
+    cin >> str;
 
-  int x; cin >> x;
-  int k ; cin >> k;
-  int tempk=k;
+    int zr=0,one=0;
+    str+=' ';//dummy  to stay in bounds
+    for(int i=0; i<n ; ++i){
 
-  for(int i =0 ; i < x; ++i){
-    int a;cin >> a; 
-
-    if(i!=x-1){
-        
-      if(k==0){
-        cout <<a<< ' ';
-        continue;
-      }
-
-      if (k-a <=0){
-          a-=k;
-          k=0;
-
-      }
-      else{
-        k-=a;    
-        a=0;
-      }
-
-      cout << a <<' ';
+      if (str[i]=='0' and str[i+1]!='0')
+        ++zr;
+      else if(str[i]=='1' and str[i+1]!='1')
+        ++one;
     }
-    else{
-      tempk-=k;
-      a+=tempk;
-      cout << a <<'\n'; 
-    }
+
+    one= min(zr,one)+1;
+
+    if(b >=0)
+      cout << n*(a+b) << '\n';
+    else 
+      cout << n*a + one*b << '\n';
+    
+
   }
-}
 
   
   return 0;
