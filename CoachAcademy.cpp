591a04c0b463292949d6e85ce0d75af1d2bdad3f
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2022-08-10 20:01:37 +0200

    sloved prob h greedy

--- CoachAcademy.cpp
+++ CoachAcademy.cpp
@@ -1,29 +1,55 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
 using ll = long long;
+
 // const ll N = 1e5;
 // int arr[N],prefix[N]{0};
 
-// greedy sheet prob D 
+// greedy sheet prob H 
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
   cin.tie(nullptr);
   cout.tie(nullptr);
 
-  int x ; cin >> x;
-  int sumpos=0, sumneg=0;
+int n ; cin >> n;
+while(n--){
 
-  for( int i=0 ; i<x ; ++i){
-    int n ; cin >> n ;
-    if(n < 0)
-      sumneg+=n;
-    else sumpos+=n;
-  }   
+  int x; cin >> x;
+  int k ; cin >> k;
+  int tempk=k;
 
-  cout << sumpos - sumneg << '\n';
+  for(int i =0 ; i < x; ++i){
+    int a;cin >> a; 
+
+    if(i!=x-1){
+        
+      if(k==0){
+        cout <<a<< ' ';
+        continue;
+      }
+
+      if (k-a <=0){
+          a-=k;
+          k=0;
+
+      }
+      else{
+        k-=a;    
+        a=0;
+      }
+
+      cout << a <<' ';
+    }
+    else{
+      tempk-=k;
+      a+=tempk;
+      cout << a <<'\n'; 
+    }
+  }
+}
+
   
   return 0;
 }
