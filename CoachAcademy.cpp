c4f421f85b098527ad69f640cdebc18935f1b63d
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2022-08-10 21:56:20 +0200

    done prob h greedy sheet

--- CoachAcademy.cpp
+++ CoachAcademy.cpp
@@ -6,41 +6,28 @@ using ll = long long;
 // const ll N = 1e5;
 // int arr[N],prefix[N]{0};
 
-// greedy sheet prob H 
-int main() {
+// greedy sheet prob E
+int main() 
   ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
   cin.tie(nullptr);
   cout.tie(nullptr);
 
-  int t,n,a,b;
-  cin >> t;
-  while(t--){
-    cin >> n;
-    cin >> a >> b;
-    string str ;
-    cin >> str;
+  string str; cin >> str;
+  bool valid=1;
+  int sz = str.size();
+  for(int i=0 ; i < sz ; ++i){
 
-    int zr=0,one=0;
-    str+=' ';//dummy  to stay in bounds
-    for(int i=0; i<n ; ++i){
-
-      if (str[i]=='0' and str[i+1]!='0')
-        ++zr;
-      else if(str[i]=='1' and str[i+1]!='1')
-        ++one;
+    if(str[i]=='0'&& valid){
+      valid=0;
+      continue;
     }
 
-    one= min(zr,one)+1;
+    if(i==sz-1 && valid )
+      break;  
 
-    if(b >=0)
-      cout << n*(a+b) << '\n';
-    else 
-      cout << n*a + one*b << '\n';
-    
-
-  }
-
+    cout << str[i];
+  }  
   
   return 0;
 }
