cf383c6ecd7f709aa38bfc53e5e86c6a7e1591c0
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2022-08-11 04:33:47 +0200

    thats it for to day done prob f sheet greedy only c left i wrong on test 10

--- CoachAcademy.cpp
+++ CoachAcademy.cpp
@@ -4,62 +4,32 @@ using namespace std;
 using ll = long long;
 
 // const ll N = 1e5;
-// int arr[N],prefix[N]{0};
+// int a[N],prefix[N]{0};
 
-// greedy sheet prob j
+// greedy sheet prob F
 int main(){
   ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
   cin.tie(nullptr);
   cout.tie(nullptr);
 
-  int t  ; cin >> t ;
-  while( t--){
-    int c1,c2,c3;
-    cin >> c1>>c2>>c3;
-    int a1,a2,a3,a4,a5;
-    cin >> a1 >> a2 >>a3 >> a4 >>a5;
-
-    if( a1>c1 or a2>c2 or a3>c3){
-      cout << "NO\n";continue;
-    }
-    if( (a1<=c1  and a2<=c2  and a3<=c3) and (a4==0 and a5==0)){
-      cout << "YES\n";continue;
-    }
-    
-    c1-=a1;c2-=a2;c3-=a3;
-    
-    if(c1!=0){
-      if(c1-a4< 0){
-        a4-=c1;
-        c1=0;
+  int t ; cin >>t;
+  while(t--){
+    int n;cin >>n;
+    string a,b; cin >> b;
+    char prv= '#';
+    for (char c :b){
+      for(char d ='1';d>= '0';--d){
+        if(c-'0'+d != prv ){
+          prv=c - '0' +d;
+          a+=d;
+          break;
+        }
       }
-      else if (c1-a4 > 0){
-        a4=0;
-        c1-=a4;
-      }
-      else {
-        c1=0;a4=0;
-      }
-    }
-    if(c2!=0){
-      if(c2-a5< 0){
-        a5-=c2;
-        c2=0;
-      }
-      else if (c2-a5 > 0){
-        a5=0;
-        c2-=a5;
-      }
-      else {
-        c2=0;a5=0;
-      }
-    }
-
-    if(a4+a5 > c3)
-      cout << "NO\n";
-    else 
-      cout << "YES\n";
+    } 
+  
+    cout <<a << '\n';
   }
+
   return 0;
 }
