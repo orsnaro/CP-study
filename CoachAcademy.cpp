4485c946c2f9210c76626810df428767dd7eb70a
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2022-08-11 15:52:36 +0200

    this solution i understand but isnt mine :(

--- CoachAcademy.cpp
+++ CoachAcademy.cpp
@@ -3,33 +3,30 @@
 using namespace std;
 using ll = long long;
 
-// const ll N = 1e5;
-// int a[N],prefix[N]{0};
+const int N = 1e6;
+int a[N];
 
-// greedy sheet prob F
-int main(){
+//sheet greedy prob C 
+//my 1st solution almost worked stopeed at test 11 
+// couldnt find test case that counter my  prev solution :(
+int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
   cin.tie(nullptr);
   cout.tie(nullptr);
 
-  int t ; cin >>t;
-  while(t--){
-    int n;cin >>n;
-    string a,b; cin >> b;
-    char prv= '#';
-    for (char c :b){
-      for(char d ='1';d>= '0';--d){
-        if(c-'0'+d != prv ){
-          prv=c - '0' +d;
-          a+=d;
-          break;
-        }
-      }
-    } 
-  
-    cout <<a << '\n';
-  }
+  int n;
+  cin >> n;
+  ll moves=0;
+
+
+  for(int i=1 ; i<=n ; ++i)
+    cin >> a[i];
+  sort(a+1,a+n+1);
+
+  for (int i=1; i <= n ; ++i)
+    moves+=abs(a[i]-i);
+  cout << moves << '\n';
 
   return 0;
 }
