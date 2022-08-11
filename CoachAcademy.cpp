916ea8c48f450b5b012571de893a64c6aa894a7d
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2022-08-11 18:53:27 +0200

    done prob A sheet SRQ

--- CoachAcademy.cpp
+++ CoachAcademy.cpp
@@ -3,16 +3,32 @@
 using namespace std;
 using ll = long long;
 
-// const int  N = 1e6;
-// int arr[N];
+const int  N = 1e6;
+int arr[N]{0};
 
-//sheet static range query prob : 
+//sheet static range query prob : A
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
   cin.tie(nullptr);
   cout.tie(nullptr);
 
- 
+  string s ;
+  cin >> s;
+  int sz=s.size();
+  arr[sz]=' ';
+  for(int i=1; i<=sz; ++i){
+    if(s[i-1]==s[i])
+      arr[i]=arr[i-1]+1;
+    else
+      arr[i]+=arr[i-1];
+  }
+
+  int n ;cin>> n ;
+  while(n--){
+    int  a,b ; cin >> a >> b;
+    cout << arr[b-1]-arr[a-1]<<'\n';
+  }
+
   return 0;
 }
