96b1457f1a66019cadcb45ab53cfe62947d23496
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2022-08-15 02:12:52 +0200

    done sheet srq prob c

--- CoachAcademy.cpp
+++ CoachAcademy.cpp
@@ -3,23 +3,42 @@
 using namespace std;
 using ll = long long;
 
-const int  N = 1e6;
-int arr[N]{0};
-
-//sheet static range query prob : B
+const int  N = 2e5+10;
+int arr[N];
+int occured[N];
+int ans[N];
+//sheet static range query prob : C
+//my code was bit  similar couldnt do it fully :(
 int main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(nullptr);
-  cin.tie(nullptr);
-  cout.tie(nullptr);
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
 
-  int myna=5;
+	int n,k,q;
+	cin >> n >> k >>q;
 
-  string s ="omar ";
-  cout << s<< endl;
-  int x;
-  cin >>x; 
+	while(n--){ 
+		int st , end; 
+		cin >> st  >> end;
+		occured[st]+=1;
+		occured[end+1]-=1;
+		
+	}
+		for(int i=1 ; i<N ; ++i)
+			occured[i]+= occured[i-1];
+		for(int i=1; i<N ; ++i){
+			if(occured[i]>=k)
+				ans[i]=ans[i-1] + 1;
+			else ans[i]=ans[i-1];
+		}
 
-  
-  return 0;
-}
+	while(q--){
+		int st2,end2;
+		cin >> st2 >> end2;
+		cout << ans[end2]-ans[st2-1] << '\n';
+	}
+		
+
+	return 0;
+} 
