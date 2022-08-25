f0e86c57b68e7bbc4e1afd73d24d6294ee65d751
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2022-08-25 04:45:11 +0200

    done prob c bitmasks time limit was really hard

--- CoachAcademy.cpp
+++ CoachAcademy.cpp
@@ -3,51 +3,39 @@
 using namespace std;
 using ll = long long;
 
-const int N = 15;
-char arr[N];
-char arr2[N];
+const int N = 25;
+int arr[N];
 
-// bitmasks  prob : F
-int main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(nullptr);
-  cin.tie(nullptr);
-  cout.tie(nullptr);
+void ans(){
+  int n, p;
+    scanf("%d %d",&n , &p);
 
-  string sent, rec;
-	cin >> sent >> rec;
-  int cntr = 0 , cntr2 =0 , notsure=0;
-	
-  for (int i = 0; i < sent.size(); ++i) {
-    if (sent[i] == '+')
-      ++cntr;
-    else
-      --cntr;
-    if (rec[i] == '+')
-      ++cntr2;
-    else if (rec[i] == '-')
-      --cntr2;
-    else
-      ++notsure;
-  }
-
-  int success = 0;
-  double couldhe = 0;
-
-  for (int msk = 0; msk < (1 << notsure); ++msk) {
-      int temp = cntr2;
-    for (int i = 0; i < notsure ; ++i) {
-      if (msk & (1 << i)) // let 1b='+' and  0b='-'
-        temp++;
-      else
-        temp--;
+    for (int i = 0; i < p; ++i) {
+      scanf("%d", &arr[i]);
     }
-    if (temp == cntr)
-      ++success;
+    bool valid = 0;
+    for (int msk = 0; msk < (1 << p); ++msk) {
+      int sum = 0;
+      for (int i = 0; i < p; ++i) {
+        if (msk & (1 << i))  // or (msk>>i) & 1
+          sum += arr[i];
+      }
+      valid |= (sum == n); //nice bit-wise technique 1st time using this;
+    }
+      if (valid) 
+        printf("YES\n");
+      else
+        printf("NO\n");
   }
 
-  couldhe = success / (double)(1 << notsure);
-  cout << fixed << setprecision(12) << couldhe << '\n';
+
+// bitmasks  prob : C
+int main() {
+  int t;
+  // cin >> t;
+  scanf("%d",&t);
+  while (t--) ans();
+    
 
   return 0;
 }
