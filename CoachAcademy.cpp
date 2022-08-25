a1b06c65d60e46d86a91085e82404d171f1fd0f1
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2022-08-25 07:18:47 +0200

    at last 10 min of sheet solved prob g  SRQ

--- CoachAcademy.cpp
+++ CoachAcademy.cpp
@@ -3,50 +3,52 @@
 using namespace std;
 using ll = long long;
 
-const int N = 1e6;
-int arr[N];
-ll unsorted[N];
-ll sorted[N];
+// const int N = 1e6;
+// int arr[N];
+// ll unsorted[N];
+// ll sorted[N];
 
-// SRQ  prob : D this sheet will end T_T
+// SRQ  prob : G this sheet will end T_T
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
   cin.tie(nullptr);
   cout.tie(nullptr);
-
-  int n;
-  cin >> n;
-  for (int i = 0; i < n; ++i) {
-    cin >> arr[i];
-    // mset.insert(arr[i]);
-    // mp.insert({arr[i],i});
-  }
-
-  unsorted[0] = arr[0];
-  for (int i = 1; i < n; ++i) {
-    unsorted[i] = unsorted[i - 1] + arr[i];
-  }
-  sort(arr, arr + n);
-  sorted[0] = arr[0];
-  for (int i = 1; i < n; ++i) {
-    sorted[i] = sorted[i - 1] + arr[i];
-  }
-
-  int m;
-  cin >> m;
-  while (m--) {
-    int type, l, r;
-    cin >> type >> l >> r;
-    switch (type) {
-    case 1:
-      cout << unsorted[r - 1] - unsorted[l - 2] << '\n';
-      break;
-    case 2:
-      cout << sorted[r - 1] - sorted[l - 2] << '\n';
-      break;
+  int q;
+  cin >> q;
+  bool is_1stbk = 1;
+  map<int, int> mp;
+  int lft = 0, right = 0;
+  while (q--) {
+    char qry;
+    cin >> qry;
+    int id;
+    cin >> id;
+    if (qry == 'L') {
+      if (is_1stbk) {
+        mp.insert({id, 0});
+        is_1stbk = 0;
+      } else {
+        lft++;
+        mp.insert({id, lft});
+      }
+    } else if (qry == 'R') {
+      if (is_1stbk) {
+        mp.insert({id, 0});
+        is_1stbk = 0;
+      } else {
+        right--;
+        mp.insert({id, right});
+      }
+    } else {
+      if (mp[id] < 0) {
+        int mn = min(abs(lft + abs(mp[id])), abs(right) - abs(mp[id]));
+        cout << mn << '\n';
+      } else {
+        int mn = min(abs(mp[id] + abs(right)), lft - mp[id]);
+        cout << mn << '\n';
+      }
     }
   }
-
   return 0;
 }
