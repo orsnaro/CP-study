7c610fcb94436f0694be5ad541a471f7f026e64c
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2022-08-25 02:38:17 +0200

    solved prob E bitmasks

--- CoachAcademy.cpp
+++ CoachAcademy.cpp
@@ -3,58 +3,42 @@
 using namespace std;
 using ll = long long;
 
-const int N = 100;
-int arr[N];
+// const int N = 100;
+// int arr[N];
 
-// bitmasks  prob : c
-int main()
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-	cin.tie(nullptr);
-	cout.tie(nullptr);
+// bitmasks  prob : E
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
 
-	int t;
-	cin >> t;
-	while (t--)
-	{
-		bool done = 0;
-		int n, p;
-		cin >> n >> p;
-		if (n == 0)
-		{
-			cout << "YES\n";
-			continue;
-		}
-
-		for (int i = 0; i < p; ++i)
-		{
-			cin >> arr[i];
-		}
-		bool invalid = 1;
-		for (int msk = 0; msk < (1 << p); ++msk)
-		{
-			int sum = 0;
-			for (int i = 0; i < p; ++i)
-			{
-				if (msk & (1 << i))
-				{ // or (msk>>i) & 1
-					sum += arr[i];
-				}
-				if (sum == n)
-				{
-					cout << "YES\n";
-					invalid = 0;
-					done = 1;
-					break;
-				}
-			}
-			if (done == 1)
-				break;
-		}
-		if (invalid)
-			cout << "NO\n";
-	}
-
-	return 0;
+  int x;
+  cin >> x;
+  vector<ll> sprlky;
+  for (int msk = 0; msk < (1 << 10); ++msk) {
+    ll temp = 0;
+    int cntr = 0;
+    for (int i = 0; i < 10; ++i) {
+      if (msk & (1 << i)) { // 0b =4 and 1b =7
+        temp *= 10;
+        temp += 7;
+        cntr++;
+      } else {
+        temp *= 10;
+        temp += 4;
+        cntr--;
+      }
+      if (cntr == 0)
+        sprlky.push_back(temp);
+    }
+  }
+  sort(sprlky.begin(), sprlky.end());
+  for (int i = 0; i < sprlky.size(); ++i) {
+    if (sprlky[i] >= x) {
+      cout << sprlky[i] << '\n';
+      return 0;
+    }
+  }
+  return 0;
 }
