cf809dba417dd37e19aec02303238ba86ad4ed64
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2022-08-27 22:36:58 +0200

    prob h bitmasks proud of it learned alot

--- CoachAcademy.cpp
+++ CoachAcademy.cpp
@@ -4,51 +4,71 @@ using namespace std;
 using ll = long long;
 
 // const int N = 1e6;
-// int arr[N];
-// ll unsorted[N];
-// ll sorted[N];
 
-// SRQ  prob : G this sheet will end T_T
+vector<char> arr;
+ll strtoint(vector<char> &vec);
+
+// bitmasks prob : H proud of it :)
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
   cin.tie(nullptr);
   cout.tie(nullptr);
-  int q;
-  cin >> q;
-  bool is_1stbk = 1;
-  map<int, int> mp;
-  int lft = 0, right = 0;
-  while (q--) {
-    char qry;
-    cin >> qry;
-    int id;
-    cin >> id;
-    if (qry == 'L') {
-      if (is_1stbk) {
-        mp.insert({id, 0});
-        is_1stbk = 0;
-      } else {
-        lft++;
-        mp.insert({id, lft});
+
+  string s;
+  cin >> s;
+  for (char x : s)
+    arr.push_back(x);
+
+  int sz = arr.size();
+
+  ll sz2 = 1 << sz - 2;
+  ll sum = 0;
+  for (ll msk = 0; msk < (1ll << ((sz * 1ll) - 1ll)); ++msk) {
+
+    vector<char> temp;
+    if (sz == 1) {
+      cout << strtoint(arr);
+      break;
+    }
+
+    for (int i = 0; i < sz - 1; ++i) {
+
+      char ch;
+      if (i == 0) {
+        ch = arr[0];
+        temp.push_back(ch);
       }
-    } else if (qry == 'R') {
-      if (is_1stbk) {
-        mp.insert({id, 0});
-        is_1stbk = 0;
+      if (msk & (sz2 >> i)) { // 1b equal '+'
+        sum += strtoint(temp);
+        temp.clear();
+        ch = arr[i + 1];
+        temp.push_back(ch);
       } else {
-        right--;
-        mp.insert({id, right});
-      }
-    } else {
-      if (mp[id] < 0) {
-        int mn = min(abs(lft + abs(mp[id])), abs(right) - abs(mp[id]));
-        cout << mn << '\n';
-      } else {
-        int mn = min(abs(mp[id] + abs(right)), lft - mp[id]);
-        cout << mn << '\n';
+        ch = arr[i + 1];
+        temp.push_back(ch);
       }
     }
+    sum += strtoint(temp);
   }
+  if (sz == 1)
+    return 0;
+
+  cout << sum << '\n';
+
   return 0;
+}
+
+ll strtoint(vector<char> &vec) {
+  int sz = vec.size();
+  ll converted2int = 0;
+  for (int i = 0; i < sz; ++i) {
+    if (vec[i] >= 48 and vec[i] <= 57) {
+      converted2int *= 10;
+      converted2int += (vec[i] - 48);
+    } else
+      break;
+  }
+
+  return converted2int;
 }
