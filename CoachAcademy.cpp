69a38d8f57720f431b1b21c2fd0efde5f567b195
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2022-08-28 04:00:57 +0200

    '

--- CoachAcademy.cpp
+++ CoachAcademy.cpp
@@ -3,32 +3,35 @@
 using namespace std;
 using ll = long long;
 
-// const int N = 1e6;
-// int arr[N];
-
+const int N = 100;
+int arr[N];
 // bitmasks prob : B
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
   cin.tie(nullptr);
   cout.tie(nullptr);
-
-  int t;
-  cin >> t;
-  while (t--) {
-    int N;
-    cin >> N;
-    if (log2(N) - (int)log2(N) == 0) {
-      cout << -1 << '\n';
-      continue;
-    }
-    for (int a = 1; a < N; ++a) {
-      if ((a^N) <= N) {
-        cout << a << ' ' << (a ^ N) << '\n';
-        break;
+  int n;
+  cin >> n;
+  int mxlen = -1;
+  for (int i = 0; i < n; ++i)
+    cin >> arr[i];
+  for (int msk = 0; msk < (1 << n); msk++) {
+    int mxval = -1;
+    vector<int> vec;
+    int prev = -1;
+    for (int i = 0; i < n; ++i) {
+      bool iszero = !(msk & ((1 << n - 1) >> i));
+      if (iszero) {
+        if (prev < arr[i]) {
+          prev = arr[i];
+          vec.push_back(prev);
+        }
       }
     }
+    int vsz = vec.size();
+    mxlen = max(mxlen, vsz);
   }
-
+  cout << mxlen << endl;
   return 0;
 }
