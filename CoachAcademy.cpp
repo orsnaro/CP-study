38d4c5d4bf7efeaa563739638ba52d69268cb77e
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2022-08-29 02:38:41 +0200

    prob h finally i got it tle two times  but i saw coach explanation to get optimized code

--- CoachAcademy.cpp
+++ CoachAcademy.cpp
@@ -3,29 +3,61 @@
 using namespace std;
 using ll = long long;
 
-// const int N = 1e6;
+const int N = 1e5;
 // int arr[N];
-// STLs II prob : C
-int main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(nullptr);
-  cin.tie(nullptr);
-  cout.tie(nullptr);
-  int N; cin>> N;
-  while(N--){
-    unordered_set <int> uno_st;
-    int ans=0;
-    int n,m; cin >> n >> m ;
-    for(int i =0; i < n; ++i){
-      int temp; cin >> temp;
-      uno_st.emplace(temp);
+
+// stls II prob : H
+int main() { // optimize time (got time limit)
+  // ios_base::sync_with_stdio(false);
+  // cin.tie(nullptr);
+  // cout.tie(nullptr);
+
+  priority_queue<int, vector<int>, greater<int>> pq;   // to sort from smaller
+  deque<int> dq; // main container
+
+  int t; cin >> t;
+  while (t--) {
+
+    int x, qry;
+    scanf("%d",&qry);
+
+    if (qry == 1) {
+      scanf("%d",&x);
+      dq.push_back(x);
+      continue;
     }
-    for(int i =0; i < m; ++i){
-      int temp; cin >> temp;
-      if(uno_st.count(temp))
-        ans++;
+    else if (qry == 2) {
+      if(!pq.empty()){
+        printf("%d\n",pq.top());
+        pq.pop();
+      }else{
+        cout << dq.front() << '\n';
+        dq.pop_front();
+      }
+      continue;
+    } 
+    else if (qry == 3) { // sort
+      // int temparr[N]={0};
+      // int pq_sz = pq.size();
+      // if (pq_sz != 0) {
+      //   pr.swap(emptier);
+      // }
+      // int i = 0;
+      // for (auto &x : dq) {
+      //   temparr[i] = x;
+      //   i++;
+      // }
+      // sort(temparr,temparr+i);
+
+      // dq.clear();
+      // // pq_sz = pq.size();
+      // for (int j = 0; j < i; ++j) 
+      //   dq.push_back(temparr[j]);
+      for (auto &x : dq)
+        pq.push(x);
+      dq.clear();
     }
-    cout << ans << '\n';
   }
+
   return 0;
 }
