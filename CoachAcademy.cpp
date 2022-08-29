a2046e6fa25e3a23fdbcd3be94fe43810223d50a
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2022-08-29 21:34:21 +0200

    took less than 20 min stld II prob E

--- CoachAcademy.cpp
+++ CoachAcademy.cpp
@@ -6,36 +6,25 @@ using ll = long long;
 // const int N = 1e6;
 // int arr[N];
 
-// stls II prob : F
-int main() { 
+// stls II prob : E
+int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
   cout.tie(nullptr);
-  
-  stack <string> stk;
-  unordered_set <string> uno_st;
-  int n; cin >> n;
 
-  while (n--) {
-    string s; cin >> s;
-    stk.push(s);
-    uno_st.insert(s);
+
+  string ref("qwertyuiopasdfghjkl;zxcvbnm,./");
+  char n; cin >> n;
+  string s; cin >> s;
+
+  int sz = s.size();
+  if (n == 'R') {
+    for (int i = 0; i < sz; ++i)
+      cout << ref[(ref.find(s[i])) - 1];
+  } else {
+    for (int i = 0; i < sz; ++i)
+      cout << ref[(ref.find(s[i])) + 1];
   }
-  
-  int sz = stk.size();
-  for ( int i = 0; i < sz; ++i ){
-    auto itr = uno_st.find(stk.top());
-    if(itr == uno_st.end()){
-      stk.pop();
-    }else{
-      uno_st.erase(itr);
-      cout << stk.top() << '\n';
-      stk.pop();
-    }
-  }
-
-  
-
 
   return 0;
 }
