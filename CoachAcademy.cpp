e7cef44eec570f27bf2a247210163bec19cf41eb
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2022-08-29 04:47:21 +0200

    prob f  stls II

--- CoachAcademy.cpp
+++ CoachAcademy.cpp
@@ -7,12 +7,35 @@ using ll = long long;
 // int arr[N];
 
 // stls II prob : F
-int main() { // optimize time (got time limit)
+int main() { 
   ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
   cout.tie(nullptr);
+  
+  stack <string> stk;
+  unordered_set <string> uno_st;
+  int n; cin >> n;
+
+  while (n--) {
+    string s; cin >> s;
+    stk.push(s);
+    uno_st.insert(s);
+  }
+  
+  int sz = stk.size();
+  for ( int i = 0; i < sz; ++i ){
+    auto itr = uno_st.find(stk.top());
+    if(itr == uno_st.end()){
+      stk.pop();
+    }else{
+      uno_st.erase(itr);
+      cout << stk.top() << '\n';
+      stk.pop();
+    }
+  }
 
   
+
 
   return 0;
 }
