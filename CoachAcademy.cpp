6b0e483e752ecc8d6fdc182ba55f0bc5325e61ef
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2022-09-01 01:30:22 +0200

    prob b backtracking

--- CoachAcademy.cpp
+++ CoachAcademy.cpp
@@ -1,17 +1,35 @@
 #include <bits/stdc++.h>
 
-using namespace std;
+using namespace std; //prob b backtracking
 using ll = long long;
 
-const int N = 110;
-int arr[N];
+const int N = 220;
+vector<char> vec1, vec2;
 
-int summation(int n) {
+bool is_less(bool less, int n) {
 
-  if (n == 0)
-    return arr[0];
+  if (n < 0)
+    return less;
 
-  return (arr[n]+ summation(n-1));
+  less = vec1[n] < vec2[n] ? 1 : 0;
+
+  if (less)
+    return is_less(less, n - 1);
+
+    return is_less(less, -1);
+}
+
+bool is_greater(bool greater, int n) {
+
+  if (n < 0)
+    return greater;
+
+  greater = vec1[n] > vec2[n] ? 1 : 0;
+
+  if (greater)
+    return is_greater(greater, n - 1);
+
+    return is_greater(greater, -1);
 }
 
 int main() {
@@ -19,19 +37,26 @@ int main() {
   cin.tie(nullptr);
   cout.tie(nullptr);
 
-  int t;
-  cin >> t;
-  int cntr = 1;
-  while (t--) {
-    int n;
-    cin >> n;
-    for (int i = 0; i < n; ++i) 
-      cin >> arr[i];
-
-    cout << "Case " << cntr << ": "  << summation(n - 1) << '\n';
-    memset(arr, 0, N);
-    ++ cntr;
+  int n;
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    char temp;
+    cin >> temp;
+    vec1.push_back(temp);
   }
+  sort(vec1.begin(), vec1.end());
+
+  for (int i = 0; i < n; i++) {
+    char temp;
+    cin >> temp;
+    vec2.push_back(temp);
+  }
+  sort(vec2.begin(), vec2.end());
+
+  if (is_less(0, n - 1) || is_greater(0, n - 1))
+    cout << "YES\n";
+  else
+    cout << "NO\n";
 
   return 0;
 }
