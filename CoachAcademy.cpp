38c55061575e63a0851dd1e9ca09ea3ffe3ad170
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2022-09-01 20:14:34 +0200

    #

--- CoachAcademy.cpp
+++ CoachAcademy.cpp
@@ -1,62 +1,43 @@
 #include <bits/stdc++.h>
 
-using namespace std; //prob b backtracking
+using namespace std;
 using ll = long long;
 
-const int N = 220;
-vector<char> vec1, vec2;
+const int N = 1e6;
+int arr[N];
 
-bool is_less(bool less, int n) {
+int is_multiple (int x ,int target , int cntr){
+  if(target <= x){
+  if(target*3 == x or target*2 == x)
+    return --cntr;
+  else if (target == x)
+  return cntr;
+  else
+  return -1;
+  }
+  
 
-  if (n < 0)
-    return less;
 
-  less = vec1[n] < vec2[n] ? 1 : 0;
-
-  if (less)
-    return is_less(less, n - 1);
-
-    return is_less(less, -1);
+  return is_multiple(x,target/6,cntr+2);
 }
 
-bool is_greater(bool greater, int n) {
 
-  if (n < 0)
-    return greater;
-
-  greater = vec1[n] > vec2[n] ? 1 : 0;
-
-  if (greater)
-    return is_greater(greater, n - 1);
-
-    return is_greater(greater, -1);
-}
-
+// bitmasks  prob : D backtacking
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
+  cin.tie(nullptr);
   cout.tie(nullptr);
 
-  int n;
-  cin >> n;
-  for (int i = 0; i < n; i++) {
-    char temp;
-    cin >> temp;
-    vec1.push_back(temp);
-  }
-  sort(vec1.begin(), vec1.end());
+int x,y,cntr = 0; 
+cin >> x >> y;
 
-  for (int i = 0; i < n; i++) {
-    char temp;
-    cin >> temp;
-    vec2.push_back(temp);
-  }
-  sort(vec2.begin(), vec2.end());
+// if (x>y){
+//   cout << -1  << endl;
+//   reutrn 0;
+// }
 
-  if (is_less(0, n - 1) || is_greater(0, n - 1))
-    cout << "YES\n";
-  else
-    cout << "NO\n";
+cout << is_multiple(x,y,cntr) << '\n';
 
   return 0;
 }
