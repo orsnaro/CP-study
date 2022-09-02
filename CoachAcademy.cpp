1f0f782da53d8696603b2e25e3c841cb80f554da
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2022-09-02 18:35:37 +0200

    add files for .txt input and calc runtime

--- CoachAcademy.cpp
+++ CoachAcademy.cpp
@@ -1,42 +1,20 @@
 #include <bits/stdc++.h>
-
+#define fastio   (ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL));                                                                 
+// bitmasks  prob : F backtacking
 using namespace std;
 using ll = long long;
-#define fastio   (ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL));                                                                 
 
 
 // const int N = 1e6;
 // int arr[N];
-ll x, target, cntr;
+// ll x, target, cntr;
 
 int is_mul(int x, int cntr) {
 
-  if (x == target)
-    return cntr;
-  // if( (target/2.0)-(target/2) != 0 or (target/3.0)-(target/3) != 0  )
-  //   return -1;
-
-  if (target)
-    if (target == x) {
-      return cntr;
-    } else if (target < x)
-      return -1;
-
-  int x2 = is_mul(x * 2, cntr + 1);
-  int x3 = is_mul(x * 3, cntr + 1);
-
-  if (x2 == -1)
-    return x3;
-  else
-    return x2;
 }
 
-// bitmasks  prob : E backtacking
 int main() {
   fastio;
-
-  cin >> x >> target;
-  cout << is_mul(x, cntr) << '\n';
 
   return 0;
 }
--- tour_of_cpp.cpp
+++ tour_of_cpp.cpp
@@ -1,27 +1,9 @@
 #include <bits/stdc++.h>
-#define fastread      (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+
 using namespace std;
-using ll = long long;
 
+int main()
+{
 
-const int N = 1e6;
-ll arr[N];
-
-// bitmasks  prob : K bitmasking
-    fastread;
-
-    int n; cin >> n;
-    int sm =0 ;
-    for (int i = 0 ; i < n; ++i){
-        cin >> arr[i];
-        sm=sm^arr[i];
-    }
-    
-    for(int i = 0; i < n ; ++i)
-        cout << (sm^arr[i]) << ' ';
-    
-
-
-
-  return 0;
+return 0;
 }
