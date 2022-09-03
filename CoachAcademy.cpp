238bf0bde45dcf3666cb8c8bf8b401e1af950f1b
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2022-09-03 05:14:15 +0200

    this gives me time limit maybe i have to do it in recursion method

--- CoachAcademy.cpp
+++ CoachAcademy.cpp
@@ -1,50 +1,64 @@
 #include <bits/stdc++.h>
 #define fastio                                                                 \
   (ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL));
-// backtacking   prob : J (didnt use backtracking)
+// backtacking   prob : H (didnt use backtracking)
 using namespace std;
 using ll = long long;
 
-const int N = 1e5;
+const int N = 12;
+int arr[N] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37};
 // ll x, target, cntr;
 
 // int is_mul(int x, int cntr) {
 
 // }
+bool prime_find(int p) {
+    for(int i = 2; i <= sqrt(p); i++)
+        if(p % i == 0) return false;
+    return true;
+}
 
 int main() {
-  fastio;
+  // fastio;
 
-  int target, n;
+  int target, cntr = 0;
+  while (scanf("%d",&target) != EOF) {
+    vector<int> vec;
+    for (int i = 0; i < target; ++i) 
+      vec.push_back(i+1);
+    
 
-  while (cin >> target >> n) {
-    int arr[N] = {0};
-    for (int i = 0; i < n; ++i)
-      cin >> arr[i];
+    cntr++;
+    if (cntr >1)
+      // cout<<'\n';
+      printf("\n");
 
-    vector<int> vec2;
-    int mx = -1;
-    for (ll msk = 0; msk < (1ll << n * 1ll); msk++) {
-      int sum = 0;
-      vector<int> vec;
-
-      for (int i = 0; i < n; ++i) {
-        ll sz = 1 << (n - 1);
-        if (msk & (sz >> i * 1ll)) {
-          vec.push_back(arr[i]);
-          sum += arr[i];
+    printf("Case %d:\n",cntr);
+    // cout << "Case " << cntr << ':' << '\n';
+    for (int i = 0; ; ++i) {
+      bool is_prime = 0;
+      for (int j = 1; j < target; ++j) {
+        int temp2 = vec[j - 1] + vec[j];
+        is_prime = prime_find(temp2);
+        if (!is_prime)
+          break;
+      }
+      if (is_prime & prime_find(vec[target - 1] + vec[0])) {
+        for (int i = 0 ; i < target; ++i){
+          // cout << vec[i];
+          printf ("%d",vec[i]);
+          if(i+1 == target)
+            break;
+          // cout << ' ';
+          printf (" ");
         }
+        printf ("\n");
+        // cout << '\n';
       }
-      if (sum <= target) {
-        mx = max(sum, mx);
-        if (sum >= mx)
-          vec2.swap(vec);
-      }
+      bool there_permutaion = next_permutation(vec.begin()+1, vec.end());
+      if (!there_permutaion)
+        break;
     }
-      for (int &x : vec2)
-        cout << x << ' ';
-      cout << "sum:" << mx << '\n';
   }
-
   return 0;
 }
