b5fe62cb627652756ca990a50ef2336508002f20
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2022-09-05 19:04:48 +0200

    finnaly ac but a could optimize it more by using my sqrt (mx) as bitset size

--- CoachAcademy.cpp
+++ CoachAcademy.cpp
@@ -2,41 +2,51 @@
 #define fastio                                                                 \
   (ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL));
 
-// num. theory I   prob : B no i doesnt OF. it TLEs -_-)
+// num. theory I   prob : B try linear sieve later )
 using namespace std;
 using ll = long long;
-
-// const int N = 1e6;
+using ull = unsigned long long;
+const ll N = 1e7;
 // int arr[N];
-unordered_map <ll,int> mp;
-vector <ll> v; 
-void sol_t_seive(ll max) {
-  for (ll p = 2; p <= max ; p++) {
-      for (ll i = p + p; i <= max; i += p){
-        bool is_wanted = mp.count(i);
-        if (is_wanted)
-          ++mp[i];
-      }
+vector<ll> v;
+vector<bool> bitv(N);
+
+void sol_t_seive( /* ll max */ ) {
+  // bitset <max + 1> btst;
+  fill(bitv.begin(), bitv.end(), true);
+  for (ll p = 2; p * p <= N; p++) {
+    if (bitv[p]) {
+      for (ull i = p * p; i <= N; i += p)
+        bitv[i] = false;
+    }
   }
-  for ( auto &x : v){
-      if(mp[x] == 3)
-        cout << "YES\n";
-      else cout << "NO\n";
+
+  for (ll &x : v) {
+    double dsquare = sqrt(x);
+    ll lsquare = sqrt(x);
+    if (x == 1)
+      cout << "NO\n";
+    else if (bitv[lsquare] && !(dsquare - lsquare))
+      cout << "YES\n";
+    else
+      cout << "NO\n";
   }
   return;
 }
 
-int main() { 
-  fastio;
-  int n; cin >> n;
-  ll mx = -1;
 
-  for ( int i = 0 ; i < n; ++i){
-    ll temp; cin >> temp;
+int main() {
+  fastio;
+  int n;
+  cin >> n;
+  // ll mx = -1;
+
+  for (int i = 0; i < n; ++i) {
+    ll temp;
+    cin >> temp;
     v.push_back(temp);
-    mp[temp] = 2;
-    mx = max(temp, mx);
+    // mx = max(temp,mx);
   }
-  sol_t_seive(mx);
+  sol_t_seive(/* mx */);
   return 0;
 }
