0139f6c8e9a8d016557e2a5aed8f8ccf491c9ee5
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2022-09-08 02:15:23 +0200

    done prob c num theo I

--- CoachAcademy.cpp
+++ CoachAcademy.cpp
@@ -1,52 +1,104 @@
+// num. theory I   prob : C hyprid of my solution with coach solution
+//not neccesary i know but cant delete my answer cuz of tle :()
 #include <bits/stdc++.h>
 #define fastio                                                                 \
   (ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL));
-
-// num. theory I   prob : B try linear sieve later )
 using namespace std;
 using ll = long long;
 using ull = unsigned long long;
-const ll N = 1e7;
-// int arr[N];
-vector<ll> v;
-vector<bool> bitv(N);
 
-void sol_t_seive( /* ll max */ ) {
-  // bitset <max + 1> btst;
-  fill(bitv.begin(), bitv.end(), true);
-  for (ll p = 2; p * p <= N; p++) {
-    if (bitv[p]) {
-      for (ull i = p * p; i <= N; i += p)
-        bitv[i] = false;
+const ll N = 1e6 +10;
+// int freq[N];
+vector<int> v(N);
+
+void seive(ll a) { // nlog(log(n))
+
+  v[1] = 1;
+  for (int i = 3; i <= a; i += 2)
+    v[i] = i;
+  for (int i = 2; i <= a; i += 2)
+    v[i] = 2;
+
+  for (ll p = 3; p * p <= a; p++) {
+    if (v[p] == p) {
+      for (ll i = p * p; i <= a; i += p) {
+        if (v[i] == i)
+          v[i] = p;
+      }
     }
   }
 
-  for (ll &x : v) {
-    double dsquare = sqrt(x);
-    ll lsquare = sqrt(x);
-    if (x == 1)
-      cout << "NO\n";
-    else if (bitv[lsquare] && !(dsquare - lsquare))
-      cout << "YES\n";
-    else
-      cout << "NO\n";
+  if (v[a] == a) {
+    cout << "1\n" << a << '\n';
+    return;
   }
+
+  vector<ll> tv;
+  int cntr = 0;
+  pair<int, ll> mx = {-1, 0};
+  while (a != 1) {
+    tv.push_back(v[a]);
+    ll temp = a;
+    a /= v[a];
+
+    if (v[temp] == v[a])
+      cntr++;
+    else {
+      cntr++;
+      if (mx.first <= cntr)
+        mx = {cntr, v[temp]};
+      cntr = 0;
+    }
+  }
+
+  cout << mx.first << '\n';
+
+  int rst = 1;
+  for (auto x : tv) {
+    if (x == mx.second and mx.first != 1) {
+      --mx.first;
+      cout << x << ' ';
+    } else
+      rst *= x;
+  }
+  cout << rst << '\n';
+
   return;
 }
 
-
 int main() {
   fastio;
-  int n;
-  cin >> n;
-  // ll mx = -1;
 
-  for (int i = 0; i < n; ++i) {
-    ll temp;
-    cin >> temp;
-    v.push_back(temp);
-    // mx = max(temp,mx);
+  int t;
+  cin >> t;
+  while (t--) {
+    ll a;
+    cin >> a;
+    
+    if (a > 100000) {
+      ll p = -1, cntr = 0, n = a;
+      for (ll i = 2; i * i <= n; ++i) {
+
+        int temp = 0;
+        for (; n % i == 0; n /= i, ++temp);
+
+        if (cntr < temp) {
+          cntr = temp;
+          p = i;
+        }
+      }
+      if (n > 1 and cntr < 1) {
+        cntr = 1;
+        p = n;
+      }
+      cout << cntr << '\n';
+      for (int i = 1; i < cntr; ++i) {
+        cout << p << ' ';
+        a /= p;
+      }
+      cout << a << '\n';
+    }else seive(a);
   }
-  sol_t_seive(/* mx */);
+
   return 0;
 }
