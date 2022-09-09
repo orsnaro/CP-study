47c7d48591dbb0ec92a6b41d127a387fd4794a62
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2022-09-09 05:01:44 +0200

    my solution to num theo 2 prob c using divisors of (a-b) worked yay !!

--- CoachAcademy.cpp
+++ CoachAcademy.cpp
@@ -1,44 +1,54 @@
 #include <bits/stdc++.h>
-// num. theory II prob : B
-#define fastio ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+// num. theory II prob : B (my modified method yay)
+#define fastio                                                                 \
+  ios_base::sync_with_stdio(false);                                            \
+  cin.tie(nullptr);                                                            \
+  cout.tie(nullptr);
 using namespace std;
-using ll =long long ;
+using ll = long long;
 
-const int  N = 1e6;
+const int N = 1e6;
 // char arr[N];
-set <ll> st;
-
-void coDiv(ll a, ll b)
-{
-    ll n = gcd(a, b);
- 
-    for (int i = 1; i <= n / i; i++) {
-        if (n % i == 0) {
-          st.insert(-i);
-          st.insert(-(n / i));
-        }
-    }
-}
+set<ll> st;
 
 int main() {
   fastio;
 
-  ll a ,b ; cin >> a >> b ;
-  coDiv (a ,b);
-
-  int t; cin >> t;
-  while (t--){
-    ll l,h; cin >> l >> h;
-    bool done = 0;
-    for ( auto &x : st){
-      if (-x <= h and -x >=l){
-        cout  << -x << '\n';
-        done = 1;
-        break;
+  ll a, b;
+  cin >> a >> b;
+  ll diff = abs(a - b);
+  if (a == b) {
+    for (int i = 1; i <= a / i; ++i) {
+      if (a % i == 0) {
+        st.insert(-i);
+        st.insert(-(a / i));
       }
     }
-    if (!done)
-      cout << "-1\n";
+  } else {
+    for (int i = 1; i <= diff / i; ++i) {
+      if (diff % i == 0 and a % i == 0 and b % i == 0) 
+        st.insert(-i);
+
+      if ((a % (diff / i)) == 0 and (b % (diff / i)) == 0)
+        st.insert(-(diff / i));
+      }
+    }
+
+    int t;
+    cin >> t;
+    while (t--) {
+      ll l, h;
+      cin >> l >> h;
+      bool done = 0;
+      for (auto &x : st) {
+        if (-x <= h and -x >= l) {
+          cout << -x << '\n';
+          done = 1;
+          break;
+        }
+      }
+      if (!done)
+        cout << "-1\n";
+    }
+    return 0;
   }
-  return 0;
-}
