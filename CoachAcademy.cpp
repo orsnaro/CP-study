f60ae8b3222efb9949619d6c1af77aa54a999d7d
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2022-09-09 04:40:32 +0200

    sheet num theo 2 prob b i use to methos store diff divisors that devides a and b also stor all gcd(a,b) divisors first worked and i think second failed cuz of casa a=b

--- CoachAcademy.cpp
+++ CoachAcademy.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+// num. theory II prob : B
 #define fastio ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
 using namespace std;
 using ll =long long ;
@@ -7,25 +8,37 @@ const int  N = 1e6;
 // char arr[N];
 set <ll> st;
 
+void coDiv(ll a, ll b)
+{
+    ll n = gcd(a, b);
+ 
+    for (int i = 1; i <= n / i; i++) {
+        if (n % i == 0) {
+          st.insert(-i);
+          st.insert(-(n / i));
+        }
+    }
+}
+
 int main() {
   fastio;
-  ll a ,b ; cin >> a >> b ;
-  ll diff = abs (a -b);
-  for ( int i = 1 ; i <= diff/i ; ++i){
-    if ( diff % i == 0 and a % i == 0 and b % i == 0){
-      st.insert(-i);
-    }
-  }
 
+  ll a ,b ; cin >> a >> b ;
+  coDiv (a ,b);
 
   int t; cin >> t;
   while (t--){
     ll l,h; cin >> l >> h;
+    bool done = 0;
     for ( auto &x : st){
-      if (-x <= h and -x >=l)
+      if (-x <= h and -x >=l){
         cout  << -x << '\n';
-      break;
+        done = 1;
+        break;
+      }
     }
+    if (!done)
+      cout << "-1\n";
   }
   return 0;
 }
