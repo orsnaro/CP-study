20ee2d6b238af1a95ecc6b17e4b323eceedbde59
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2022-09-14 14:55:10 +0200

    deleted some repeated + old cpp files

--- CoachAcademy.cpp
+++ CoachAcademy.cpp
@@ -1,45 +1,18 @@
 #include <bits/stdc++.h>
-// num. theory II prob : G
+// num. theory II prob :
 #define fastio                                                                 \
   ios_base::sync_with_stdio(false);                                            \
   cin.tie(nullptr);                                                            \
   cout.tie(nullptr);
-
 using namespace std;
 using ll = long long;
 
-const int N = 1e6;
-int arr[N];
+
+// const int N = 1e6;
+// int arr[N];
 
 int main() {
   fastio;
-
-  int t;
-  cin >> t;
-  while (t--) {
-    int n;
-    cin >> n;
-    int temp = (N + 1);
-
-    for (int i = 0; i < n; ++i) {
-      cin >> arr[i];
-      if (arr[i] < temp)
-        temp = arr[i];
-    }
-
-    int k = 0;
-
-    for (int i = 0; i < n; ++i) {
-      if (arr[i] == temp)
-        continue;
-      k = __gcd(k, arr[i] - temp);
-    }
-
-    if (!k)
-      cout << -1 << '\n';
-    else
-      cout << k << '\n';
-  }
 
   return 0;
 }
