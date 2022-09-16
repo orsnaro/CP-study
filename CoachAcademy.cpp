cf684fe364f766346eaaa7b27b1ede5b6d94db47
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2022-09-16 02:19:08 +0200

    forgot that probs solution must be in one file so we revert new CP temp and not use cp.h for now + SOLVED PROB F sorting II sheet

--- CoachAcademy.cpp
+++ CoachAcademy.cpp
@@ -1,10 +1,38 @@
-#include "comp.h"
-// sorting II prob : H
+// sorting II prob : F
+#include <bits/stdc++.h>
+#define fastio                                                                 \
+  ios_base::sync_with_stdio(false);                                            \
+  cin.tie(nullptr);                                                            \
+  cout.tie(nullptr);
+#define f first
+#define s second
+using ll = long long;
+
+using namespace std;
+const int N = 1e6;
+int arr[N];
+vector<pair<int, int>> v;
 
 int main() {
   fastio;
-  
 
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; ++i)
+      cin >> arr[i];
+    sort(arr, arr + n);
+
+    if (n & 1)
+      cout << arr[n / 2] << ' ';
+
+    for (int i = n / 2 - 1; i >= 0; i--)
+      cout << arr[i] << ' ' << arr[n - 1 - i] << ' ';
+
+    cout << '\n';
+  }
 
   return 0;
 }
