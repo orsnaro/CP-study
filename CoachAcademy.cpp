9ab917533f640a61d460dd5990f4bd10cc26070d
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2022-09-19 17:49:52 +0200

    add 2 files one pending probplem dont know the error and a new mini project + friends project dir

--- CoachAcademy.cpp
+++ CoachAcademy.cpp
@@ -1,30 +1,22 @@
-// sorting II prob : J
+// sheet sorting I prob : 
 #include <bits/stdc++.h>
-#define f first
-#define s second
+#define fastio                                                                 \
+  ios_base::sync_with_stdio(false);                                            \
+  cin.tie(nullptr);                                                            \
+  cout.tie(nullptr);
+
+using namespace std;
 using ll = long long;
 
-const int N = 1e6;
+// const int N = 1e6;
+// int arr[N];
 
 int main() {
+  fastio;
+  int t = 1;
+  // cin >> t;
+  while (t--) {
 
-  int t; cin >> t;
-  while ( t-- ){
-    // int temp[100000+5] = {0};
-    int arr[100000 + 10] = {0};
-    int a1,a2,a3;
-    int n; cin >> n;
-    // bool neg = 1, pve = 1;
-    for (size_t i = 0; i < n; i++){
-      cin >> arr[i];
-    }
-    sort( arr,arr + n);
-    a1 = arr[0]*arr[1]*arr[n-1]*arr[n-2]*arr[n-3];
-    a2 = arr[n-1]*arr[n-2]*arr[n-3]*arr[n-4]*arr[n-5];
-    a3 = arr[1]*arr[2]*arr[0]*arr[3]*arr[n-1];
-    int mx = max(a1,a2);
-    mx = max(mx,a3);
-    cout << mx << '\n';
   }
   return 0;
 }
