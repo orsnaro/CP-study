30ed06bb33d9256cbb34a8afd3228ad0069d1cff
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2022-09-27 01:42:40 +0200

    #

--- CoachAcademy.cpp
+++ CoachAcademy.cpp
@@ -1,4 +1,4 @@
-// sheet : genral 4 (15)  prob: E
+// sheet : genral 4 (15)  prob: C
 #include <bits/stdc++.h>
 
 #define fastio                                                                 \
@@ -17,30 +17,7 @@ int main() {
     int t = 1;
     cin >> t;
     while (t--) {
-        int n;
-        cin >> n;
-        int grid[n][n] = {0};
-        fill(&grid[0][0], &grid[0][0] + (sizeof(grid)/sizeof(int)), 0);
-        int i = 0;
-        bool is_od = n % 2;
-        while (i < n) {
-            grid[i][i] = 1;
-            grid[i][n - 1 - i] = 1;
-            i++;
-        }
-        i = 0;
 
-        if (is_od) {
-            grid[n / 2][n - 1] = 1;
-            grid[0][n / 2] = 1;
-        }
-
-        for (int j = 0; j < n; j++) {
-            for (int k = 0; k < n; k++)
-                cout << grid[j][k] << ' ';
-
-            cout << '\n';
-        }
     }
     return 0;
 }
