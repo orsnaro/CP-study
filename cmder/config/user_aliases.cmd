67c71dc3eb4bbbab2f2b43326cdb1ea219d4f127
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2022-09-28 20:37:58 +0200

    # prob B sheet 15 done

---
+++ cmder/config/user_aliases.cmd
@@ -0,0 +1,1 @@
+comp=start https://vjudge.net/contest $t start https://codeforces.com/contests  $t code C:\Users\OmarPc\repo_CPP_COMP_study\CoachAcademy.cpp $t start C:\Users\OmarPc\AppData\Local\Programs\pomotroid\Pomotroid.exe  
