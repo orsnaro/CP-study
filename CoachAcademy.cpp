4e089787003a71289eb966c6b08b9f008b1c53f8
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2022-09-28 19:03:11 +0200

    #

--- CoachAcademy.cpp
+++ CoachAcademy.cpp
@@ -1,14 +1,14 @@
-// sheet : general  (13)  prob: E
+// sheet : general  (13)  prob: D
 #include <bits/stdc++.h>
-
 #define fastio                                                                 \
     ios_base::sync_with_stdio(false);                                          \
     cin.tie(nullptr);                                                          \
     cout.tie(nullptr);
 
+#define f first
+#define s second
 using namespace std;
 using ll = long long;
-
 const int N = 1e6;
 int arr[N];
 
@@ -17,20 +17,7 @@ int main() {
     int t = 1;
     cin >> t;
     while (t--) {
-        int n;
-        cin >> n;
-        ll sum = 0, ans = 0;
-        int smdtrc, equal;
-        for (size_t i = 0; i < n; i++) {
-            int tmp;
-            cin >> tmp;
-            sum += tmp;
-        }
-        smdtrc = sum % n;
-        int smdtrc2 = smdtrc;
-        equal = (sum - smdtrc) / n;
-        ans = 1ll*(n - smdtrc2) * smdtrc2;
-        cout << ans << '\n';
+        // code here
     }
     return 0;
 }
