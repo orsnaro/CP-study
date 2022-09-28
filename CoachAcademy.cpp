882e6f7865e9abe0fc68aa85a325e4947c605f03
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2022-09-28 21:04:52 +0200

    #

--- CoachAcademy.cpp
+++ CoachAcademy.cpp
@@ -11,36 +11,14 @@ using namespace std;
 using ll = long long;
 const int N = 1e6, M = 1e7;
 int arr[N];
-bitset<20100> bst;
-vector<int> primes;
-
-void sieve() {
-    // 0 means a prime
-    bst[0] = bst[1] = 1;
-
-    for (size_t i = 2; i < 20100 / i; i++)
-        if (!bst[i])
-            for (size_t j = i * i; j < 20100; j += i)
-                bst[j] = 1;
-
-    for (int i = 0; i < 20100; i++)
-        if (bst[i] == 0)
-            primes.push_back(i);
-}
 
 int main() {
     fastio;
     int t = 1;
     cin >> t;
 
-    sieve();
     while (t--) {
-        int d;
-        cin >> d;
-        int p = *upper_bound(primes.begin(), primes.end(), d);
-        int q = *lower_bound(primes.begin(), primes.end(), p + d);
-        ll ans = 1ll * p * q;
-        cout << ans << '\n';
+        
     }
     return 0;
 }
--- cmder/config/user_aliases.cmd
+++ cmder/config/user_aliases.cmd
@@ -1,1 +1,3 @@
 comp=start https://vjudge.net/contest $t start https://codeforces.com/contests  $t code C:\Users\OmarPc\repo_CPP_COMP_study\CoachAcademy.cpp $t start C:\Users\OmarPc\AppData\Local\Programs\pomotroid\Pomotroid.exe  
+spot= start https://open.spotify.com/search  
+sound= start https://soundcloud.com  
