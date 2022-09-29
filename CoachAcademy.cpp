bd0a55eb2625576e813fc8ba779bea39314bb83d
omar rashad salem <77698890+orsnaro@users.noreply.github.com>
2022-09-29 16:54:36 +0200

    done prob A sheet 16

--- CoachAcademy.cpp
+++ CoachAcademy.cpp
@@ -1,4 +1,4 @@
-// sheet : general  (13)  prob: D
+// sheet : general  (16)  prob: A
 #include <bits/stdc++.h>
 #define fastio                                                                 \
     ios_base::sync_with_stdio(false);                                          \
@@ -15,10 +15,32 @@ int arr[N];
 int main() {
     fastio;
     int t = 1;
-    cin >> t;
-
+    // cin >> t;
     while (t--) {
-        
+      vector <int> v;
+      int n; cin >> n;
+      int cntr = 1, prev = -1 , crnt = -1;
+      for (size_t i = 0; i < n; i++) {
+        int tmp; cin >> tmp;
+        if ( i == 0)
+          prev = crnt = tmp;
+
+        prev = crnt ;
+        crnt = tmp;
+
+        if ( tmp == 1 and i != 0 ){
+          cntr++;
+          v.push_back(prev);
+        }
+
+        if  ( i == n -1 ){
+          v.push_back(tmp);
+          break;
+        }
+      }
+      cout << cntr << '\n';
+      for (auto &x : v)
+        cout << x <<' ';
     }
     return 0;
 }
