c9f3b797fc04b679a93aac6fc43cc9cffa8d293d
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2022-10-03 05:31:22 +0200

    # nice one did it almost my self ( took prac like month ago)

--- CP.cpp
+++ CP.cpp
@@ -1,4 +1,4 @@
-// sheet : 5  prob: E 
+// sheet : 10  prob: H
 #include <bits/stdc++.h>
 #define fastio                                                                 \
     ios_base::sync_with_stdio(false);                                          \
@@ -10,42 +10,54 @@
 using namespace std;
 using ll = long long;
 const int N = 1e6, M = 1e7;
-int arr[N], cnt[N];
-ll tot[N];
-pair<pair<int, int>, int> op[N];
+int arr[N];
+ll fact(int x) {
+
+    if (x == 1)
+        return 1;
+    return x * fact(x - 1);
+}
+ll nCr(int z_cnt) {
+    if (z_cnt == 0)
+        return 1;
+
+    ll nfact = fact(z_cnt + 1);
+    ll rfact = fact(z_cnt);
+    int dnrfact = 1;
+
+    ll _ncr = nfact / (rfact * dnrfact);
+
+    return _ncr;
+}
 
 int main() {
     fastio;
     int t = 1;
     // cin >> t;
     while (t--) {
-        int n, m, k;
-        cin >> n >> m >> k; // arr size = n , op num = m , query num =k
-        for (size_t i = 1; i <= n; i++)
-            cin >> arr[i];
-        for (size_t i = 1; i <= m; i++)
-            cin >> op[i].F.F >> op[i].F.S >> op[i].S;
+        bool cnt_on = 0, fnd1 = 0;
+        int cnt_0 = 0;
+        int n;
+        ll ans = 1;
+        cin >> n;
+        int tmp, prev;
+        for (size_t i = 0; i < n; i++) {
+            prev = tmp;
+            cin >> tmp;
+            if (i == 0)
+                prev = tmp;
 
-        for (size_t i = 0; i < k; i++) { // partial sum on opertation freq
-            int x, y;
-            cin >> x >> y;
-            ++cnt[x];
-            --cnt[y + 1];
+            if (tmp == 1) {
+                fnd1 = 1;
+                ans *= 1ll * nCr(cnt_0);
+                cnt_0 = 0;
+            } else if (!tmp and fnd1)
+                cnt_0++;
         }
-
-        for (size_t i = 1; i <= m;i++)
-        { // prefix sum on op freq 'cnt' + partial sum on final sum to
-                    // main arr
-            cnt[i] += cnt[i - 1];
-            tot[op[i].F.F] += 1ll * op[i].S * cnt[i];
-            tot[op[i].F.S + 1] -= 1ll * op[i].S * cnt[i];
-        }
-
-        for (size_t i = 1; i <= n; i++)
-        { // prefix sum  on tot[N] + cout final arr[n]
-            tot[i] += tot[i - 1];
-            cout << arr[i] + tot[i] << ' ';
-        }
+        if (!fnd1)
+            cout << "0\n";
+        else
+        cout << ans << '\n';
     }
 
     return 0;
