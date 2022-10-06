4696efc50fe9ab62395adb49e4bfe9c1ca65bfec
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2022-10-06 23:44:46 +0200

    # done sheet 9 prob J #

--- CP.cpp
+++ CP.cpp
@@ -1,4 +1,4 @@
-// sheet : 9  prob: G
+// sheet : 9  prob: E
 #include <bits/stdc++.h>
 #define fastio                                                                 \
     ios_base::sync_with_stdio(false);                                          \
@@ -9,42 +9,19 @@
 #define S second
 using namespace std;
 using ll = long long int;
-const int N = 1e6 + 30, M = 1e7;
-int spf [N];
-int arr[N];
-
+const int N = 1e8 + 3, M = 1e7;
+bool prime[N];
 void seive(){
-    for (size_t i = 0; i < N; i++){
-        spf[i] = i;
-        if ( !(i & 1) )
-            spf[i] = 2;
-    }
-    
-    for (size_t i = 3; i < N / i; i++){
-        if ( spf[i] == i ){
-            for (ll j = 1ll * i * i; j < N; j += i)
-                if ( spf[j] == j )
-                    spf [j] = i;
-        }
-    }
-    
-    return;
-}
+    prime[0] = prime[1] = 1; 
 
-int  do_fact( int x ){
-    fill ( arr ,arr + N ,0);
-    int mxspf = -1 ;
-    while ( x != 1){
-        mxspf = max ( mxspf , spf[x]);
-        arr[spf[x]]++;
-        x = x / spf[x];
-    }   
-
-    int cnt = 1;
-    for  (int i = 0; i <= mxspf ; ++i)
-        cnt *= (arr[i] + 1) ;
     
-    return cnt;
+    for (int i = 2; i <= N / i; ++i)
+        if (!prime[i]){
+            for ( ll j = 1ll * i * i; j <= N; j += i )
+                prime[j] = 1; 
+        }else continue;
+
+    return;           
 }
 
 int main() {
@@ -53,16 +30,16 @@ int main() {
     // cin >> t;
     seive();
     while (t--) {
-        ll sum = 0;
-        int  a,b,c; cin >> a >> b >> c;
-        for (size_t ai = 1; ai <= a; ai++)
-            for (size_t bi = 1; bi <= b; bi++)
-                for (size_t ci = 1; ci <= c; ci++){
-                    int tmp = ai * bi * ci;
-                    sum += do_fact(tmp);
-                }
-        ll ans = sum % 1073741824;
-        cout << ans << '\n';
+        int cnt = 0; 
+        for (int i = 2; i < N; i++){
+            if (prime[i])
+                continue;
+            
+            ++cnt;
+            if ( cnt % 100 == 1)
+                cout << i << '\n';
+        }
+        
     }
     return 0;
 }
