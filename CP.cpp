22bdcad10a0cc9c25c1782c7a7c166eaf4fc06a8
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2022-10-07 22:16:16 +0200

    #

--- CP.cpp
+++ CP.cpp
@@ -1,4 +1,4 @@
-// sheet : 9  prob: j
+// sheet : 14  prob: G
 #include <bits/stdc++.h>
 #define fastio                                                                 \
     ios_base::sync_with_stdio(false);                                          \
@@ -9,33 +9,45 @@
 #define S second
 using namespace std;
 using ll = long long int;
-const int N = 1e6, M = 1e8, mod = 1e9 + 7;
+const int N = 1e6, M = 1e8;
 // int arr[N];
-int pfreq[N];
+// int d[8][2]{
+//     {+1,0 },
+//     {0,+1},
+//     {-1,0},
+//     {0,-1},
+//     {+1,+1},
+//     {-1,-1},
+//     {+1,-1},
+//     {-1,+1}
+// };
 
 int main() {
     fastio;
     int t = 1;
     // cin >> t;
     while (t--) {
-        int n;
-        cin >> n;
-        int ans = 1;
-        for (size_t i = 1; i <= n; i++) {
-            int tmp = i;
-            for (int j = 2; j <= n / j; ++j)
-                while (tmp % j == 0) {
-                    tmp /= j;
-                    pfreq[j]++;
-                }
-            if (tmp > 1)
-                pfreq[tmp]++;
+        int r1, c1, r2, c2;
+        int arr[3] = {0}; // rook bishop king
+        cin >> r1 >> c1 >> r2 >> c2;
+        // rook
+        if (c1 == c2 or r1 == r2)
+            arr[0] = 1;
+        else
+            arr[0] = 2;
+
+        // bishop
+        if (  ((r1 + c1) & 1) == ((r2 + c2 ) & 1) ){
+            if (abs(r1 - r2) == abs(c1 - c2))
+                arr[1] = 1;
+            else
+                arr[1] = 2;
         }
 
-        for (size_t i = 2; i <= n; i++)
-            ans = (ans * 1ll * (pfreq[i] + 1)) % mod;
+        //king 
+        arr[2] = max ( abs (r1 - r2) , abs(c1 - c2));
 
-        cout << ans << '\n';
+        cout << arr[0] << ' ' << arr[1] << ' ' << arr[2] << '\n';
     }
     return 0;
 }
