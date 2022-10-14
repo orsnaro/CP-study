057a7bedaf26a7586a7c74a51f6312ff83e064c1
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2022-10-14 19:24:24 +0200

    # added div4 prob to solve later #

--- CP.cpp
+++ CP.cpp
@@ -1,4 +1,4 @@
-// sheet : sort 1  prob: G
+// problem link : Codeforces Round #827 (Div. 4) : e
 #include <bits/stdc++.h>
 #define fastio                                                                 \
     ios_base::sync_with_stdio(false);                                          \
@@ -9,46 +9,14 @@
 #define S second
 using namespace std;
 using ll = long long int;
-const int N = 1e6 + 5, M = 1e8;
-// int arr[N];
+const int N = 1e6, M = 1e8;
+ll arr[N];
 
 int main() {
-    fastio;
+    // fastio;
     int t = 1;
     cin >> t;
     while (t--) {
-        int n, k;
-        cin >> n >> k;
-        deque<int> a;
-        deque<int> b;
-        for (int i = 0; i < n; ++i){
-            int tmp; cin >> tmp;
-            a.push_back(tmp);
-        }
-        for (int i = 0; i < k; ++i){
-            int tmp; cin >> tmp;
-            b.push_back(tmp);
-        }
-
-        sort(a.begin(), a.end());   // from smallest
-        sort(b.rbegin(), b.rend()); // from biggest
-
-        ll ans = 0;
-        while (!b.empty() and b.back() == 1) {
-            ans += 2ll * a.back();
-            a.pop_back();
-            b.pop_back();
-        }
-        for (size_t i = 0; i < b.size(); i++) {
-            ans += a.back();
-            a.pop_back();
-            ans += a.front();
-            a.pop_front();
-
-            for (int j = 0; j < b[i] - 2; j++)
-                a.pop_front();
-        }
-        cout << ans << '\n';
     }
     return 0;
 }
