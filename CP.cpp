85bd916fc329b8b80c028e0b923c8a7648ec0718
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2022-10-27 01:00:20 +0200

    #

--- CP.cpp
+++ CP.cpp
@@ -1,4 +1,4 @@
-// PROB :https://codeforces.com/problemset/problem/372/A
+// PROB :
 #include <bits/stdc++.h>
 #define fastio                                                                 \
     ios_base::sync_with_stdio(false);                                          \
@@ -10,32 +10,13 @@
 using namespace std;
 using ll = long long int;
 const int N = 1e5, M = 1e8;
-int arr[N];
-vector<int> v;
+// int arr[N];
+
 int main() {
     fastio;
     int t = 1;
     // cin >> t;
     while (t--) {
-        int n, t;
-        cin >> n >> t;
-        for (int i = 0; i < n; ++i) {
-            int tmp; cin >> tmp;
-            v.push_back(tmp);
-        }
-
-        int cnt = 0 , j = 0 ,sum = 0;
-        for ( int i = 0; i < n; ++i){
-            sum += v[i];
-            if ( sum <= t )
-                cnt ++;
-            else{
-                sum -= v[j];
-                j++;
-            }
-        
-        } 
-        cout << cnt << '\n';
 
     }
     return 0;
--- cmder/config/user_aliases.cmd
+++ cmder/config/user_aliases.cmd
@@ -1201,3 +1201,4 @@ tele=  "C:\Users\OmarPc\AppData\Roaming\Telegram Desktop\Telegram.exe"
 stube=start https://www.youtube.com/results?search_query=$1+$2+$3+$4+$5+$6+$7+$8+$9  
 comp=cd /d c:\users\OmarPc\repo_CPP_COMP_study $t start  https://codeforces.com/contests $t code cp.cpp $t start  C:\Users\OmarPc\AppData\Local\Programs\pomotroid\Pomotroid $t start  https://commons.wikimedia.org/wiki/File:Ascii-proper-color.svg#/media/File:Ascii-proper-color.svg $t calc  
 crn=g++ "$*cpp" -Wall -Wextra -g -o active $t @echo active.exe is runing .. $t active.exe $t del active.exe  
+py=python -u $*  
