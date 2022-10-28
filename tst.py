1fb44cf5329810929f7b91b377bfd39ed34588ec
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2022-10-28 03:19:24 +0200

    # my first prob using python : took 2 hours to learn basics + inro to oop py approx #

--- tst.py
+++ tst.py
@@ -1,11 +1,27 @@
 
+def minion_game(string):
+    strsz = len(string)
+    vowls = ['I', 'O', 'U', 'A', 'E']
+    allsubs = []
+    Kevin = 0
+    Stuart = 0
 
-age = 20
-float_var = 19.23
-first_name = "mosh"
-is_bool = False
-print( " please enter your age  and name : ")
-first_name = input ( "\n name : \n ")
-age = int(input( " \n your age : \n"))
+    for i in range(0, strsz, 1):
+        is_vowl = False
+        if vowls.count(string[i]) == 1:
+            Kevin += strsz -i
+        else:
+            Stuart += strsz - i 
 
-print (" your name : %s \n your age is : %d \n" %(first_name,age))
+    # print (allsubs) , print ( len(allsubs))
+    if Kevin > Stuart:
+        print(f"Kevin {Kevin}")
+    elif Stuart > Kevin:
+        print(f"Stuart {Stuart}")
+    else:
+        print(f"Draw")
+
+
+if __name__ == '__main__':
+    s = input()
+    minion_game(s)
