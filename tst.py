9a895dbdc0a5758bc765e790f4e96f72780215a8
omar rashad salem <77698890+orsnaro@users.noreply.github.com>
2022-10-31 19:37:09 +0200

    #

--- tst.py
+++ tst.py
@@ -6,6 +6,3 @@ first_name = "mosh"
 is_bool = False
 print( " please enter your age  and name : ")
 first_name = input ( "\n name : \n ")
-age = int(input( " \n your age : \n"))
-
-print (" your name : %s \n your age is : %d \n" %(first_name,age))
