803e813c1fb78249d883e1bfe43aa640ba469da2
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2023-01-31 01:48:29 +0200

    # fix indent in vs code  and add to shorts vs code section #

--- CP.cpp
+++ CP.cpp
@@ -1,9 +1,9 @@
-// prob:  
+// prob:
 #include <bits/stdc++.h>
 #define fastio                                                                 \
-    ios_base::sync_with_stdio(false);                                          \
-    cin.tie(nullptr);                                                          \
-    cout.tie(nullptr);
+		ios_base::sync_with_stdio(false);                                          \
+		cin.tie(nullptr);                                                          \
+		cout.tie(nullptr);
 
 #define F first
 #define S second
@@ -12,12 +12,12 @@ using ll = long long;
 const int N = 1e6, M = 1e8;
 // int arr[N];
 
-int main() {
-    fastio;
-    int t = 100;
-    // cin >> t;
-    while (t--) {
-        cout << "omar";
-    }
-    return 0;
+int main(void) {
+	fastio;
+	int t = 1;
+		// cin >> t ;
+		while (t--) {  
+		// CODE HERE 
+		}
+		return 0;
 }
