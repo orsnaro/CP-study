3fdf5b5ef5f676d00a44262468c1c37c1a48820b
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2023-03-07 11:50:35 +0200

    # some jpg images that i might need #

--- CP.cpp
+++ CP.cpp
@@ -1,24 +1,30 @@
 // prob:
 #include <bits/stdc++.h>
-#define fastio                                                                 \
-		ios_base::sync_with_stdio(false);                                          \
-		cin.tie(nullptr);                                                          \
-		cout.tie(nullptr);
+using namespace std;
 
+#define fastio                                                              \
+	ios_base::sync_with_stdio(false);                                        \
+	cin.tie(nullptr);                                                        \
+	cout.tie(nullptr);
 #define F first
 #define S second
-using namespace std;
 using ll = long long;
 const int N = 1e6, M = 1e8;
 // int arr[N];
 
+
 int main(void) {
+	// freopen("in.txt","r",stdin);
 	fastio;
+
 	int t = 1;
-		// cin >> t ;
-		while (t--) {  
-		// CODE HERE 
-		cout << "omar";
-		}
-		return 0;
+	// cin >> t ;
+
+	cout << pq.size() << endl;
+
+	while (t--) {  
+		
+	}
+
+	return 0;
 }
