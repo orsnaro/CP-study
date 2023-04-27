d85de26b2ba27d5eefab62d0859840ad7d708b2a
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2023-04-27 15:24:15 +0200

    # add xampp to cmder bin + st= start ..  alias  #

--- cmder/config/user_aliases.cmd
+++ cmder/config/user_aliases.cmd
@@ -3257,3 +3257,4 @@ cm=cmder
 parkorg= cd /d C:\Users\OmarPc\repo_Smart_parking_system\  
 swb= start https://www.google.com/search?q=$1+$2+$3+$4+$5+$6+$7+$8+$9  
 phporg= cd c:\xampp\htdocs\  
+st=start $*  
