7ef9fbbcf4cbcec91e5a51722405f45fb771177f
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2023-06-16 00:37:42 +0300

    # add to bin speedtest.exe from okla + add script for  netbalance to limit pc  bandwidth#

--- cmder/config/user_aliases.cmd
+++ cmder/config/user_aliases.cmd
@@ -19,8 +19,8 @@ upa= git push --all
 g= git $*  
 a= alias $*  
 fb= start https://www.facebook.com/  
-desk= chdir /d c:\Users\OmarPc\Desktop\ $t explorer .  
-games= cd /d "C:\Users\OmarPc\Documents\game fast\" $t explorer .  
+desk= chdir /d c:\Users\%USERNAME%\Desktop\ $t explorer .  
+games= cd /d "C:\Users\%USERNAME%\Documents\game fast\" $t explorer .  
 link= cd /d C:\ProgramData\Microsoft\Windows\Start Menu\Programs  
 monk= start https://monkeytype.com/  
 in= cat input.txt  
@@ -43,20 +43,20 @@ interporg=cd /d C:\Users\%USERNAME%\repo_CRAFTING_INTERPRETERS_study
 tree= tree /f  
 hex=xxd -c 16 $*  
 chex=xxd -c 16 -i $*  
-cpplng=clang++ $* -Wall -Wextra -g -o active $t C:\Users\OmarPc\repo_CPP_COMP_study\cmder\config\check_file_exist.bat  
+cpplng=clang++ $* -Wall -Wextra -g -o active $t C:\Users\%USERNAME%\repo_CPP_COMP_study\cmder\config\check_file_exist.bat  
 hddf= cd /d f:  
 grp= grep  -i -n -H -G  $*  
-embedorg=cd /d C:\Users\OmarPc\repo_embedded-C  
+embedorg=cd /d C:\Users\%USERNAME%\repo_embedded-C  
 sqlite= sqlite3  
 sql= sqlite3  
-env= cd /d C:\Users\OmarPc\py_env\  
+env= cd /d C:\Users\%USERNAME%\py_env\  
 cm=cmder  
 st=start $*  
 d=exit  
 en=echo $*  
 tsk=tasklist $1 $b sort  
 klterm= taskkill /im WindowsTerminal.exe /f  
-jp= start C:\Users\OmarPc\win_trm_mnmzd.exe $t jupyter-lab  
+jp= start C:\Users\%USERNAME%\win_trm_mnmzd.exe $t jupyter-lab  
 cpp=g++ $* -Wall -Wextra -g -o active $t check_file_exist.bat  
 allgitstat= allgitstat.bat  
 allgitdiff= allgitdiff.bat  
@@ -64,8 +64,8 @@ allgitall= allgitall.bat
 ls=ls --show-control-chars -F --color $*  
 parkremote=start https://github.com/Gehad-AboElmagd/smart_parking_system  
 find=tasklist  $b sort $b  grep -i  "$* *"  
-keyorg= cd /d C:\Users\OmarPc\repo_KeyRec_tool  
-exe-keyrec=pyinstaller --name=KeyRec-Asda --specpath=C:\Users\OmarPc\repo_KeyRec_tool\dump --distpath=C:\Users\OmarPc\repo_KeyRec_tool\KeyRec-tool\KeyRec-Asda_V2.1.0B\ --onefile --icon=C:\Users\OmarPc\repo_KeyRec_tool\dump\KeyRec.ico C:\Users\OmarPc\repo_KeyRec_tool\source\keyrec_1window_V2.1.0B.py  
+keyorg= cd /d C:\Users\%USERNAME%\repo_KeyRec_tool  
+exe-keyrec=pyinstaller --name=KeyRec-Asda --specpath=C:\Users\%USERNAME%\repo_KeyRec_tool\dump --distpath=C:\Users\%USERNAME%\repo_KeyRec_tool\KeyRec-tool\KeyRec-Asda_V2.1.0B\ --onefile --icon=C:\Users\%USERNAME%\repo_KeyRec_tool\dump\KeyRec.ico C:\Users\%USERNAME%\repo_KeyRec_tool\source\keyrec_1window_V2.1.0B.py  
 asdarec= KeyRec-Asda  
 kltsk=taskkill /im "$*"  /f  
 remotes=start https://github.com/  
@@ -79,7 +79,7 @@ logme=git log --all --graph  --decorate --stat --author="Omar Rashad Salem"
 ffind=dir "\*$**" /s  
 ffindhere=dir "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\*$**" /s  
 movdis= mmt /MoveWindow 1 Process Discord.exe  
-orsorg= cd C:\Users\OmarPc\repo_Orsnaro  
+orsorg= cd C:\Users\%USERNAME%\repo_Orsnaro  
 phone="C:\Program Files\WindowsApps\SAMSUNGELECTRONICSCoLtd.SamsungFlux_4.9.704.0_x64__wyx1vj98g3asy\SamsungFlow.exe"  
 dis=powershell.exe -Command "Start-Process powershell -WindowStyle Hidden -ArgumentList 'Start-Process opendiscord_2nd_monitor.bat -WindowStyle Hidden'" $t mmt /MoveWindow 1 Process "Discord.exe"  /WindowLeft -5  /WindowTop 0  /WindowWidth 900   /WindowHeight 1580  
 twt=start https://www.twitch.tv/  
@@ -88,7 +88,7 @@ brave="C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe" https
 bravecog="C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe" --incognito https://www.google.com/search?q=$1+$2+$3+$4+$5+$6+$7+$8+$9  
 s=start https://www.google.com/search?q=$1+$2+$3+$4+$5+$6+$7+$8+$9  
 what=chrome https://web.whatsapp.com/ $t mmt /MoveWindow 1  Process "chrome.exe" /WindowTop 0 /WindowLeft -10 /WindowWidth 910 /WindowHeight 1600  
-tele="C:\Users\OmarPc\AppData\Roaming\Telegram Desktop\Telegram.exe" $t mmt  /MoveWindow 1  Process "Telegram.exe" /WindowTop -10 /WindowLeft 0 /WindowWidth 910 /WindowHeight 1600  
+tele="C:\Users\%USERNAME%\AppData\Roaming\Telegram Desktop\Telegram.exe" $t mmt  /MoveWindow 1  Process "Telegram.exe" /WindowTop -10 /WindowLeft 0 /WindowWidth 910 /WindowHeight 1600  
 spot="C:\Program Files\WindowsApps\SpotifyAB.SpotifyMusic_1.210.760.0_x86__zpdnekdrzrea0\Spotify.exe" $t  mmt /MoveWindow 1  Process "Spotify.exe" /WindowTop 0 /WindowLeft -10 /WindowWidth 910 /WindowHeight 1600  
 schrome=chrome "https://www.google.com/search?q=$1+$2+$3+$4+$5+$6+$7+$8+$9" $t mmt /MoveWindow 1  Process "chrome.exe" /WindowTop 0 /WindowLeft -10 /WindowWidth 910 /WindowHeight 1600  
 gpt4="C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe" --incognito --app=https://chat.forefront.ai/ $t mmt /MoveWindow 1 Title "Forefront Chat" /WindowLeft -10 /WindowTop 0 /WindowWidth 917 /WindowHeight 1600  
@@ -96,6 +96,6 @@ gpt="C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe" --app=h
 sound=chrome https://soundcloud.com  
 bard="C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe" --app=https://bard.google.com/  
 gptp="C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe" --app=https://poe.com/ChatGPT  
-botorg=cd /d C:\Users\OmarPc\repo_Discord_gpt_bot\  
-parkorg=cd /d C:\Users\OmarPc\repo_Smart_parking_system\  
-oldorg=cd /d C:\Users\OmarPc\repo_Old_projects_stash  
+botorg=cd /d C:\Users\%USERNAME%\repo_Discord_gpt_bot\  
+parkorg=cd /d C:\Users\%USERNAME%\repo_Smart_parking_system\  
+oldorg=cd /d "C:\Users\%USERNAME%\repo_Old_projects_stash"  
