c8317eff553735aaa12b23a4b68aaff1f73b3d22
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2023-06-20 19:30:12 +0300

    # done problem // prob: https://codeforces.com/contest/1843/problem/A (joined last 24 min)

--- CP.cpp
+++ CP.cpp
@@ -1,30 +1,38 @@
-// prob:
+// prob: https://codeforces.com/contest/1843/problem/A (joined last 24)
 #include <bits/stdc++.h>
 using namespace std;
 
-#define fastio                                                              \
-	ios_base::sync_with_stdio(false);                                       \
-	cin.tie(nullptr);                                                       \
-	cout.tie(nullptr);
+#define fastio                                                                 \
+   ios_base::sync_with_stdio(false);                                           \
+   cin.tie(nullptr);                                                           \
+   cout.tie(nullptr);
 #define F first
 #define S second
 using ll = long long;
 const int N = 1e6, M = 1e8;
-// int arr[N];
-
+int arr[N];
 
 int main(void) {
-	// freopen("in.txt","r",stdin);
-	fastio;
+   // freopen("in.txt","r",stdin);
+   fastio;
 
-	int t = 1;
-	// cin >> t ;
+   int t = 1;
+   cin >> t;
 
+   while (t--) {
+		int sum = 0;
+      int n;
+      cin >> n;
+      for (int i = 0; i < n; i++) {
+	 		int temp;
+			cin >> temp;
+	 		arr[i] = temp;
+      }
+		sort(arr , arr + n);
+		for(int i = 0; i < ( n - i - 1) ; i++)
+			sum += arr[n - i - 1] - arr[i];
+		cout << sum << '\n';
+   }
 
-	while (t--) {  
-		
-		
-	}
-
-	return 0;
+   return 0;
 }
