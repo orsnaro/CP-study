cf18fa057bb7ee95fd964bbf35149e285ed26759
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2023-06-21 19:00:30 +0300

    # WHATS WRONG? : https://www.codechef.com/START95D/problems/ALTTAB#

--- CP.cpp
+++ CP.cpp
@@ -1,4 +1,4 @@
-// prob: https://www.codechef.com/START95D/problems/TRANCHAIN
+// prob: https://www.codechef.com/START95D/problems/ALTTAB
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -10,28 +10,32 @@ using namespace std;
 #define S second
 using ll = long long;
 const int N = 1e6, M = 1e8;
-string arr[N];
+int arr[N];
+vector<string> v;
 
 int main(void) {
    // freopen("in.txt","r",stdin);
    fastio;
 
    int t = 1;
-   cin >> t;
+   // cin >> t;
 
    while (t--) {
-		int freq[4] = {};
-		int n; cin >> n; // 1 A 2 B 3 AB 4 O
-		int sum = 0;
-		for(int i = 0; i < n; i++){
-			cin >> arr[i];
-			if ( arr[i] == "A" ) freq[0]++;
-			else if (arr[i] == "B") freq[1]++;
-			else if (arr[i] == "AB") freq[2]++;
-			else freq[3]++;
+		
+		int n; cin >> n;
+		for (int i = 0; i < n; i++){
+			string tmp; cin >> tmp;
+			int sz = tmp.size();
+			tmp = tmp.substr(sz - 2 , 2);
+
+			auto pos = find(v.begin() , v.end() , tmp);
+			if ( pos != v.end() )
+				v.erase(pos);
+			v.push_back(tmp);
 		}
-		sum += freq[3] + max(freq[0] , freq[1]) + freq[2];
-		cout << (sum == 0 ? 1 : sum ) << '\n'; 
+		reverse(v.begin() , v.end());
+		for( auto &x : v)
+			cout << x;
    }
 
    return 0;
