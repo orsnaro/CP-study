575e686d925388dbd20d517bec963302f50ab464
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2023-07-05 00:18:41 +0300

    # done : https://codeforces.com/contest/236/problem/A #

--- CP.cpp
+++ CP.cpp
@@ -1,5 +1,5 @@
-// prob: 
-// https://codeforces.com/contest/567/problem/A
+// prob: https://codeforces.com/contest/236/problem/A
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -23,19 +23,10 @@ int main(void) {
 
 
    while (t--) {
-		int n; cin >> n;
-		for (size_t i = 0; i < n; i++) cin >> arr[i];
-
-		cout << abs(arr[0] - arr[1])  << ' ' << abs(arr[0] - arr[n-1]) << '\n'; // mini maxi
-		for (size_t i = 1; i < n - 1; i++) {//its always ASC could leave the abs()
-			int mn = min( abs(arr[i] - arr[i-1]) , abs(arr[i] - arr[i+1]) );
-			int mx = max( abs(arr[i] - arr[n-1]) , abs(arr[i] - arr[0]) );
-			cout << mn << ' ' << mx << '\n';
-		}
-		// cout and directly avoid OF
-		cout << abs(arr[n-1] - arr[n-2]) << ' ' << abs(arr[n-1] - arr[0]) << '\n';
-
-		
+		set <char> st;
+		string tmp; cin >> tmp;
+		for ( auto &c : tmp) st.insert(c);
+		cout << (st.size() % 2 == 0 ? "CHAT WITH HER!" : "IGNORE HIM!") << '\n';
    }
 
    return 0;
