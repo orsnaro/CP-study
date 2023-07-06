510c39b85691e3ec1b7fa95742a753e887b949d1
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2023-07-06 23:58:39 +0300

    # passes sample and one test case : // https://www.codechef.com/problems/CAESAR #

--- CP.cpp
+++ CP.cpp
@@ -18,18 +18,27 @@ int main(void) {
    int t = 1;
    cin >> t;
    while (t--) {
-
 		int n; cin >> n;
 		string s , t , u ; cin >> s >> t >> u;
+		string ans;
 		int k;
 		if ( s[0] > t[0])k = ('z'- s[0]) + (s[0] - 'a') + 1;
 		else k = abs(s[0] - t[0]);
 
+
+
+
 		for (int  i = 0; i < n; i++){
-			if ( (u[i] + k)  % 'z' == 0 ) u[i] = u[i] + k ;
-			else u[i] = 'a' + ( (u[i] + k) % 'z' ) - 1 ;
+			int mod = (u[i] + k)  % 'z';
+			if ( mod == 0 ) ans += char(u[i] + k) ;
+			else {
+				if ( u[i] + k  < 'z' )
+				ans += char(u[i] + k) ; 
+				else 
+				ans += char('a' + mod - 1);
+			}
 		}
-		cout << u << '\n';
+		cout << ans << '\n';
     
    }
 
