ceb1d4456e88b53cf1fa0945aa22a91ae6eb82cf
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2023-07-06 23:05:09 +0300

    # todo: q3.cpp  done : https://codeforces.com/problemset/problem/767/A #

--- q3.cpp
+++ q3.cpp
@@ -1,8 +1,8 @@
-// prob: https://codeforces.com/problemset/problem/767/A
- 
+// prob: 
+
 #include <bits/stdc++.h>
 using namespace std;
- 
+
 #define fastio                                                                 \
    ios_base::sync_with_stdio(false);                                           \
    cin.tie(nullptr);                                                           \
@@ -12,36 +12,36 @@ using namespace std;
 using ll = long long;
 const int N = 1e5, M = 1e8;
 // int arr[N];
- 
+
 int main(void) {
    // freopen("in.txt","r",stdin);
    fastio;
    int t = 1;
-   // cin >> t;
+   cin >> t;
    while (t--) {
-		int n; cin >> n;
-		int cnt = n;
-		priority_queue <int> pq; 
-		for (size_t i = 0; i < n; i++)
-		{
-			int tmp; cin >> tmp;
-			if ( tmp != cnt ){
-				pq.push(tmp);
-				cout << "\n";
-				// cout << "\n";
-			} else {
-				cout << tmp << ' ';
-				cnt --;
-				while ( !pq.empty() and pq.top() == cnt ) {
-					cnt --;
-					cout << pq.top() << ' ';
-					pq.pop();
-				}
-				if (cnt != 0) cout << "\n";
-			}
+
+    int n; cin >> n;
+    string s , t , u ; cin >> s >> t >> u;
+
+		int k = -1;
+		if ( s[0] > t[0]){
+			k = ('z'- s[0]) + (s[0] - 'a') + 1;
+		}else{
+			k = abs(s[0] - t[0])
 		}
-		
+
+    for (int  i = 0; i < n; i++)
+    {
+        if ( (u[i] + k) <=  'z') {
+          u[i] = u[i] + k ;
+        }else {
+          int z = (u[i] + k) % 'z';
+          u[i] = 'a' + (z-1);
+        }
+    }
+    cout << u << '\n';
+    
    }
- 
+
    return 0;
 }
