07622929a63f09d84714f9dc8b075b481af2fe4f
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2023-07-11 00:46:52 +0300

    # done (missed one special case then saw a solution) = https://codeforces.com/contest/610/problem/B #

--- CP.cpp
+++ CP.cpp
@@ -1,5 +1,5 @@
 
-
+// https://codeforces.com/contest/610/problem/B
 
 
 
@@ -24,6 +24,7 @@ using namespace std;
 using ll = long long;
 const int N = 1e5, M = 1e9;
 // int arr[N];
+deque <ll> dq;
 
 int main(void) {
    // freopen("in.txt","r",stdin);
@@ -31,6 +32,33 @@ int main(void) {
    int t = 1;
    // cin >> t;
    while (t--) {
+		int n; cin >> n;
+		ll mn = LONG_LONG_MAX;
+		for (int i = 0; i < n; i++)
+		{
+			ll tmp; cin >> tmp;
+			dq.push_back(tmp);
+			mn = min(mn , tmp);
+		}
+		ll ans = n * (mn*1ll); // not to OF
+		vector <int> pos;
+		for (int i = 0; i < n; i++)
+			if ( dq[i] - mn == 0 ){
+				pos.push_back(i);
+			}
+		
+		int mx = -1 ;
+		for (int  i = 1; i < pos.size() ; i++)
+			mx = max ( pos[i] - pos[i-1] - 1, mx);
+
+		
+		
+		if ( mx > n + pos[0] - pos[pos.size() - 1]  - 1)
+			ans += mx;
+		else
+			ans += n + pos[0] - pos[pos.size() -1] -1;
+
+		cout << ans << '\n';
    }
 
    return 0;
