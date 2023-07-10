672bb647fe05b1cd1440dc0ad94b578245062af6
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2023-07-10 20:29:00 +0300

    # done : https://codeforces.com/contest/266/problem/A #

--- CP.cpp
+++ CP.cpp
@@ -1,9 +1,4 @@
-
-// https://codeforces.com/contest/265/problem/A
-
-
-
-
+// https://codeforces.com/contest/266/problem/A
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -17,8 +12,6 @@ using namespace std;
 using ll = long long;
 const int N = 1e5, M = 1e9;
 // int arr[N];
-vector <ll> v;
-map <ll , int> mp;
 
 int main(void) {
    // freopen("in.txt","r",stdin);
@@ -26,27 +19,17 @@ int main(void) {
    int t = 1;
    // cin >> t;
    while (t--) {
-		string s , t ; cin >> s >> t;
-		deque <char>  st1 ,st2 ;
+      int n;
+      cin >> n;
+      int ans = 0;
+      string s;
+      cin >> s;
 
-		int ssz = s.size();
-		int tsz = t.size();
-
-		for (int i = 0; i < ssz; i++)st1.push_back(s[i]);
-		for (int i = 0; i < tsz; i++)st2.push_back(t[i]);
-
-		int ans = 1;
-		while ( !st2.empty() ){
-
-			if ( st1.front() == st2.front() ){
-				st1.pop_front();
+      for (int i = 1; i < n; i++) 
+			 if ( s[i] == s[i - 1])
 				ans ++;
-			}
 
-			st2.pop_front();
-		}
-		cout << ans << '\n';
-
+	cout << ans << '\n';
    }
 
    return 0;
