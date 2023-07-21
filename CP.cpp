4ce5750ecaf48abf1b4712fd573c50587c931b6b
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2023-07-21 17:05:16 +0300

    # add new dir 'helpers\' and moved so helping sources and docs to it#

--- CP.cpp
+++ CP.cpp
@@ -1,8 +1,8 @@
-// https://codeforces.com/contest/492/problem/B
 
 
 
 #include <bits/stdc++.h>
+// #include <stdio.h>
 using namespace std;
 
 #define fastio                                                                 \
@@ -23,28 +23,7 @@ int main(void) {
    int t = 1;
    // cin >> t;
    while (t--) {
-		int n; cin >> n; 
-		ll l; cin >> l;
-		vector <ll> v;
-		for (int i = 0; i < n; i++){
-			ll tmp; cin >> tmp;
-			v.push_back(tmp);
-		}
-		sort( v.begin() , v.end() );
-		double ans = -1;
 
-		for (int i = 0; i < n - 1; i++)
-			ans = max(ans , (v[i  + 1] - v[i]) / double(2) );
-		
-		auto it = find(v.begin() , v.end() , 0);
-		if ( it == v.end() )
-			ans = max (ans , double(v[0]));
-
-		it = find(v.begin() , v.end() , l);
-		if ( it == v.end() )
-			ans = max(ans , double(l - v[n - 1]));
-		
-		cout << fixed << setprecision(10) << ans << '\n';
    }
    return 0;
 }
