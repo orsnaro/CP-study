627f31363b0cdf24121578b5a180d2ebaf6bfaa7
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2023-07-21 19:13:59 +0300

    #todo: https://codeforces.com/contest/1850/problem/E #

--- CP.cpp
+++ CP.cpp
@@ -1,6 +1,6 @@
 
 
-
+// https://codeforces.com/contest/1850/problem/E
 #include <bits/stdc++.h>
 // #include <stdio.h>
 using namespace std;
@@ -16,13 +16,49 @@ using ll = long long;
 const int N = 2e5 + 5, M = INT_MAX;
 const ll LM= LONG_LONG_MAX;
 // int arr[N];
+ll solve ( ll a , ll b  , ll c){
+    ll discriminant = b*b - 4*a*c;
+    double x1 = 0;
+	 double x2 = 0;
+    if (discriminant > 0) {
+        x1 = (-b + sqrt(discriminant)) / (2*a);
+        x2 = (-b - sqrt(discriminant)) / (2*a);
+		  return (x1 > x2 ? x1 : x2);
+        
+    }
+    
+    else if (discriminant == 0) {
+        x1 = -b/(2*a);
+		  return x1;
+    }
+
+    else {
+		return -1 ;
+    }
+}
 
 int main(void) {
    // freopen("in.txt","r",stdin);
    fastio; // disable when using 'printf() , scanf()'
    int t = 1;
-   // cin >> t;
+   cin >> t;
    while (t--) {
+		int n; cin >> n;
+		ll c; cin >> c;
+		ll p_sum = 0;
+		ll sqrt_sum = 0;
+		for (int i = 0; i < n; i++)
+		{
+			ll tmp; cin >> tmp;
+			p_sum += tmp;
+			sqrt_sum += sqrt(tmp);
+		}
+		ll a = 4*n;
+		ll C = p_sum - c;
+		ll b = 4 * sqrt_sum;
+		ll ans = solve ( a, b, C);
+		cout << ans << '\n';
+
 
    }
    return 0;
--- todo_probs/prob_no_5.cpp
+++ todo_probs/prob_no_5.cpp
@@ -1,53 +1,65 @@
-// https://www.codechef.com/START98D/problems/BLAST3 (NOT COMPLETED)
 
 
-
+// https://codeforces.com/contest/1850/problem/E
 #include <bits/stdc++.h>
+// #include <stdio.h>
 using namespace std;
 
 #define fastio                                                                 \
    ios_base::sync_with_stdio(false);                                           \
    cin.tie(nullptr);                                                           \
    cout.tie(nullptr);
+
 #define F first
 #define S second
 using ll = long long;
-const int N = 1e5 + 5, M = INT_MAX;
+const int N = 2e5 + 5, M = INT_MAX;
 const ll LM= LONG_LONG_MAX;
+// int arr[N];
+ll solve ( ll a , ll b  , ll c){
+    ll discriminant = b*b - 4*a*c;
+    double x1 = 0;
+	 double x2 = 0;
+    if (discriminant > 0) {
+        x1 = (-b + sqrt(discriminant)) / (2*a);
+        x2 = (-b - sqrt(discriminant)) / (2*a);
+		  return (x1 > x2 ? x1 : x2);
+        
+    }
+    
+    else if (discriminant == 0) {
+        x1 = -b/(2*a);
+		  return x1;
+    }
 
-
+    else {
+		return -1 ;
+    }
+}
 
 int main(void) {
    // freopen("in.txt","r",stdin);
-   fastio;
+   fastio; // disable when using 'printf() , scanf()'
    int t = 1;
    cin >> t;
    while (t--) {
 		int n; cin >> n;
-		string s; cin >> s;
-
-		if ( s.size() % 2 == 0){
-			int half = s.size() / 2 ;
-			string s1 = s.substr(0, half);
-			stringg s2 = s.substr(half , half);
-			if ( s1 == s2 ) {cout << "YES"; continue;}
-			else{
-				if (s.size() <= 3){cout << "NO"; continue;}
-
-			}
-		}else{
-			int half = s.size() / 2 ;
-			string s1 = s.substr(0, half);
-			string s2 = s.substr(half + 1 , half);
-
-			if ( s1 == s2){cout << "YES"; continue;}
-			else{
-				if (s.size() <= 3){cout << "NO"; continue;}
-			}
+		ll c; cin >> c;
+		ll p_sum = 0;
+		ll sqrt_sum = 0;
+		for (int i = 0; i < n; i++)
+		{
+			ll tmp; cin >> tmp;
+			p_sum += tmp;
+			sqrt_sum += sqrt(tmp);
 		}
-		
-		
-   }
+		ll a = 4*n;
+		ll C = p_sum - c;
+		ll b = 4 * sqrt_sum;
+		ll ans = solve ( a, b, C);
+		cout << ans << '\n';
 
+
+   }
    return 0;
 }
