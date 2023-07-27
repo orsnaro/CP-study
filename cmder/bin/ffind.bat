aac567274231fca0d86e40c6fe5617605ddbae3a
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2023-07-27 05:18:35 +0300

    # quick fix: changes comments in .bat mus have rem before them not '::'

--- cmder/bin/ffind.bat
+++ cmder/bin/ffind.bat
@@ -8,27 +8,28 @@ set "strtMsg=Start Searching in Drive"
 set "endMsg=Done Searching in Drive"
 set "alphabet=ABCDEFGHIJKLMNOPQRSTUVWXYZ"
   
-
 for /L %%a in (0,1,25) do (
 
-	 :: retrieve letter
+	 rem retrieve letter : it expands alphabet variable using '!' 
+	 rem then uses slicing operator '~' to slice (start,size) -> (%%a, 1) 
+	 rem this gets only one char at crnt index of for loop!
 	set crntDrive=!alphabet:~%%a,1!
-	
+
 	if exist !crntDrive!: (
 		
 		cd /d !crntDrive!:
 	
 		echo.
 		echo [93m [93m===================================================================[0m
-		echo [93m [93m=                [91m%strtMsg%[96m'!crntDrive!'[93m                     =[0m        
+		echo [93m [93m=                   [91m%strtMsg%[96m'!crntDrive!'[93m                    =[0m        
 		echo [93m [93m===================================================================[0m	
 		echo.
 		
-		dir "\*%toFind%*" /s
+		dir "\*%toFind%" /s
 		
 		echo.
 		echo [93m [93m===================================================================[0m
-		echo [93m [93m=               [92m%endMsg%[96m'!crntDrive!'[93m                      =[0m   
+		echo [93m [93m=                  [92m%endMsg%[96m'!crntDrive!'[93m                     =[0m   
 		echo [93m [93m===================================================================[0m	
 		echo.
 		
@@ -36,8 +37,6 @@ for /L %%a in (0,1,25) do (
 	
 	
 )
-
-
 
 cd /d %oldpath%
 endlocal
