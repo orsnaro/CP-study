068ce14da6e017663bfc6c49421ce85b0c174dc8
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2023-09-12 18:05:16 +0300

    #Done: https://codeforces.com/contest/734/problem/A #

---
+++ done_probs/prob_no_36.cpp
@@ -0,0 +1,44 @@
+
+
+// https://codeforces.com/contest/734/problem/A
+
+
+
+// #include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
+#define Txtio   freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
+#define fastio                                                                 \
+   ios_base::sync_with_stdio(false);                                           \
+   cin.tie(nullptr);                                                           \
+   cout.tie(nullptr);
+
+#define F first
+#define S second
+using ll = long long;
+using ull = unsigned long long;
+const ll LM = LONG_LONG_MAX;
+const int N = 2e6 + 5, M = INT32_MAX;
+int arr[N];
+
+int main(void) {
+	// Txtio;
+   fastio; // disable fastio when using 'printf() , scanf()'
+   int t = 1;
+   // cin >> t;
+   while (t--) {
+		int n; cin >> n;
+		char s[n + 1];
+		int a = 0 , d = 0;
+		for (size_t i = 0; i < n; i++)
+		{
+			cin >> s[i];
+			if ( s[i] == 'A') a++;
+			else d ++;
+		}
+
+		if ( a == d ) cout << "Friendship\n";
+		else cout << (a > d ? "Anton\n" : "Danik\n");
+   }     
+	return 0;  
+}
