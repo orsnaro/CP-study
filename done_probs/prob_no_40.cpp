a541d804661e0ee3ac8834573274ac4f8fbe14ba
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2023-09-18 00:36:11 +0300

    #Done: https://codeforces.com/contest/791/problem/A #

---
+++ done_probs/prob_no_40.cpp
@@ -0,0 +1,39 @@
+
+  
+// https://codeforces.com/contest/791/problem/A
+
+
+
+
+
+
+// #include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
+#define Txtio   freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
+#define fastio                                                                 \
+   ios_base::sync_with_stdio(false);                                           \
+   cin.tie(nullptr);                                                           \
+   cout.tie(nullptr);
+
+#define F first
+#define S second
+using ll = long long;
+using ull = unsigned long long;
+const ll LM = LONG_LONG_MAX;
+const int N = 2e6 + 5, M = INT32_MAX;
+int arr[N];
+
+int main(void) {
+	// Txtio;
+   fastio; // disable fastio when using 'printf() , scanf()'
+   int t = 1;
+   // cin >> t;
+   while (t--) {
+		double a , b; cin >> a >> b;
+
+		int ans = (log(b/a)/log(3/2.0)) + 1;
+		cout << ans << '\n';
+   }     
+	return 0;  
+}
