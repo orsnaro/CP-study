572612f15e476916c2761da36b07c5c518eaf0bd
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2023-09-20 17:58:03 +0300

    #Done: https://www.codechef.com/START101D/problems/SMARTER #

---
+++ done_probs/prob_no_51.cpp
@@ -0,0 +1,35 @@
+  
+
+
+
+
+//#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
+#define Txtio   freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
+#define fastio                                                                 \
+   ios_base::sync_with_stdio(false);                                           \
+   cin.tie(nullptr);                                                           \
+   cout.tie(nullptr);
+
+#define F first
+#define S second
+using ll = long long;
+using ull = unsigned long long;
+const ll LM = LONG_LONG_MAX;
+const int N = 2e6 + 5, M = INT32_MAX;
+int arr[N];
+
+int main(void) {
+	// Txtio;
+   fastio; // disable fastio when using 'printf() , scanf()'
+   int t = 1;
+   cin >> t;
+   while (t--) {
+		double l , a , b; cin >> l >> a >> b;
+		int t1 = ceil(l / a);
+		int t2 = ceil(l / b);
+		cout << ( (t1 - t2 - 1) > -1 ? t1 - t2 -1  : -1  )<< '\n';
+   }     
+	return 0;  
+}
