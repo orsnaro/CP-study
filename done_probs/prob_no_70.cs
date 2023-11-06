f4adb988e0552664233ecb9c6a1e655e6867a474
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2023-11-06 17:30:08 +0200

    #Done: https://www.hackerrank.com/challenges/a-very-big-sum/problem?isFullScreen=true #

---
+++ done_probs/prob_no_70.cs
@@ -0,0 +1,57 @@
+﻿// you can use top level statments from C# 9 and onwards 
+// i.e.( no need to specify class and Main() in the main file but other files need to have the full structure for compiler to figure the main file )
+			// Console.WriteLine("Hello, World!"); //(you can run this directly with out the whoe down struct)
+
+//prob: https://www.hackerrank.com/challenges/a-very-big-sum/problem?isFullScreen=true
+
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'aVeryBigSum' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts LONG_INTEGER_ARRAY ar as parameter.
+     */
+
+    public static long aVeryBigSum(List<long> ar)
+    {
+        var n = Convert.ToInt32(Console.ReadLine());
+        long ans = ar.Sum();
+        return ans;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int arCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<long> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt64(arTemp)).ToList();
+
+        long result = Result.aVeryBigSum(ar);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
