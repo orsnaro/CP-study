6c805244cd93e15c00df3e69a55b1a6d2d381f97
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2023-11-06 17:33:09 +0200

    # add `cp.cs` main file for solving c# problems (the project and solution files are ignored jsut create them one time via vs code or studio)##

---
+++ Cp.cs
@@ -0,0 +1,5 @@
+﻿// you can use top level statments from C# 9 and onwards 
+// i.e.( no need to specify class and Main() in the main file but other files need to have the full structure for compiler to figure the main file )
+			// Console.WriteLine("Hello, World!"); //(you can run this directly with out the whoe down struct)
+
+//prob:
