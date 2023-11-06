b66a9d5d603e52943b1f9c9b8cc07216b62476f0
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2023-11-06 17:27:57 +0200

    #Done: https://www.hackerrank.com/challenges/simple-array-sum/problem?isFullScreen=true #

---
+++ done_probs/prob_no_68.cs
@@ -0,0 +1,62 @@
+﻿// you can use top level statments from C# 9 and onwards 
+// i.e.( no need to specify class and Main() in the main file but other files need to have the full structure for compiler to figure the main file )
+			// Console.WriteLine("Hello, World!"); //(you can run this directly with out the whoe down struct)
+
+//prob: https://www.hackerrank.com/challenges/simple-array-sum/problem?isFullScreen=true
+
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'simpleArraySum' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY ar as parameter.
+     */
+
+    public static int simpleArraySum(List<int> ar)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        int ans = 0;
+        
+        foreach( var val in ar){
+            ans += val;
+        }
+            
+         return ans;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int arCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt32(arTemp)).ToList();
+
+        int result = Result.simpleArraySum(ar);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
