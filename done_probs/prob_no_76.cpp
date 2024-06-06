150d704a5d433c8190dc93aa3b7afc1ce706ce60
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2024-06-07 00:51:18 +0300

    #Done: https://www.hackerrank.com/challenges/time-conversion/problem?isFullScreen=true #

---
+++ done_probs/prob_no_76.cpp
@@ -0,0 +1,34 @@
+
+
+
+
+
+
+
+
+
+// #include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
+#define Txtio   freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
+#define fastio                                                                 \
+   ios_base::sync_with_stdio(false);                                           \
+   cin.tie(nullptr);                                                           \
+   cout.tie(nullptr);
+
+#define F first
+#define S second
+using ll = long long;
+using ull = unsigned long long;
+const ll LM = LONG_LONG_MAX;
+const int N = 2e5 + 5, M = INT32_MAX;
+
+int main(void) {
+	// Txtio;
+   fastio; // disable fastio when using 'printf() , scanf()'
+   int t = 1;
+   // cin >> t;
+   while (t--) {
+	}
+	return 0;  
+}
