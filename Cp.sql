8f59bdeb1a8726c5010e7bc291c61e2ba6a4b39f
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2026-05-16 18:04:11 +0300

    #Done: https://leetcode.com/problems/recyclable-and-low-fat-products/?envType=study-plan-v2&envId=top-sql-50 #

---
+++ Cp.sql
@@ -0,0 +1,6 @@
+-- problem: https://leetcode.com/problems/recyclable-and-low-fat-products/?envType=study-plan-v2&envId=top-sql-50
+
+select "product_id" from "products" where "low_fats" = 'Y' and "recyclable" = 'Y';
+-- or
+--select product_id from Products where low_fats = 'Y' and recyclable = 'Y';
+
