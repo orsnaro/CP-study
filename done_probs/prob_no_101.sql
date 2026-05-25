3f8cd0b50d9b1710b0db0240dcbfc4289d5a2481
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2026-05-25 19:03:55 +0300

    typo: last commit missed the FROM line I somehow deleted it

--- done_probs/prob_no_101.sql
+++ done_probs/prob_no_101.sql
@@ -2,7 +2,9 @@
 -- the visit id that does not exist in transactions we will get the corresponding user id for.
 -- then count how many times this same customer id did a visit with no transaction (visit_id that is in visits but not in transactions) (my sol uses JOIN other 2 can use sub query via NOT IN , NOT EXISTS)
 
+
 select v.customer_id, Count(v.visit_id) as count_no_trans
+from Visits as v
 left join Transactions as t on v.visit_id = t.visit_id --where keyword can replace this line *IF* it was inner join  then and it with any other where statementsa
 where t.transaction_id is null
 group by v.customer_id
